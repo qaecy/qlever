qaecy/qlever

commit 079b7080dade63094b61f696c56c3bca94f6e5af

- We can now extract Prefixes from UTf-8 Strings. This is needed and used in the text index for Prefix Filters. - Adapted e2e-tests to unicode features
- TODO: Many more unit tests
- Proper Unicode Normalization and verification of the PrefixLength.

src/index/Index.Text.cpp
@@ -97,7 +97,7 @@ size_t Index::passContextFileForVocabulary(string const& contextFile) {
   LOG(INFO) << "Making pass over ContextFile " << contextFile
             << " for vocabulary." << std::endl;
   ContextFileParser::Line line;
-  ContextFileParser p(contextFile);
+  ContextFileParser p(contextFile, _textVocab.getLocaleManager());
   ad_utility::HashSet<string> items;
   size_t i = 0;
   while (p.getLine(line)) {
@@ -120,7 +120,7 @@ void Index::passContextFileIntoVector(const string& contextFile,
   LOG(INFO) << "Making pass over ContextFile " << contextFile
             << " and creating stxxl vector.\n";
   ContextFileParser::Line line;
-  ContextFileParser p(contextFile);
+  ContextFileParser p(contextFile, _textVocab.getLocaleManager());
   size_t i = 0;
   // write using vector_bufwriter
 
@@ -131,7 +131,7 @@ void Index::passContextFileIntoVector(const string& contextFile,
   // this has to be repeated completely here because we have the possibility to
   // only add a text index. In that case the Vocabulary has never been
   // initialized before
-  _vocab = Vocabulary<CompressedString>();
+  _vocab = Vocabulary<CompressedString, TripleComponentComparator>();
   readConfiguration();
   _vocab.readFromFile(_onDiskBase + ".vocabulary",
                       _onDiskLiterals ? _onDiskBase + ".literals-index" : "");
@@ -397,14 +397,24 @@ void Index::calculateBlockBoundaries() {
   // 4) word.substring(0, MIN_PREFIX_LENGTH) is different from the next.
   // A block boundary is always the last WordId in the block.
   // this way std::lower_bound will point to the correct bracket.
+  if (_textVocab.size() == 0) {
+    LOG(WARN) << "You are trying to call calculateBlockBoundaries on an empty "
+                 "text vocabulary\n";
+    return;
+  }
+  const auto& locManager = _textVocab.getLocaleManager();
+  auto currentLenAndPrefix = LocaleManager::getUTF8Prefix(
+      _textVocab[0].value().get(), MIN_WORD_PREFIX_SIZE);
   for (size_t i = 0; i < _textVocab.size() - 1; ++i) {
     // we need foo.value().get() because the vocab returns
     // a std::optional<std::reference_wrapper<string>> and the "." currently
     // doesn't implicitly convert to a true reference (unlike function calls)
-    if (_textVocab[i].value().get().size() < MIN_WORD_PREFIX_SIZE ||
-        (_textVocab[i + 1].value().get().size() < MIN_WORD_PREFIX_SIZE) ||
-        _textVocab[i].value().get().substr(0, MIN_WORD_PREFIX_SIZE) !=
-            _textVocab[i + 1].value().get().substr(0, MIN_WORD_PREFIX_SIZE)) {
+    auto nextLenAndPrefix = LocaleManager::getUTF8Prefix(
+        _textVocab[i + 1].value().get(), MIN_WORD_PREFIX_SIZE);
+    if (currentLenAndPrefix.first < MIN_WORD_PREFIX_SIZE ||
+        (nextLenAndPrefix.first < MIN_WORD_PREFIX_SIZE) ||
+        locManager.compare(currentLenAndPrefix.second, nextLenAndPrefix.second,
+                           LocaleManager::Level::PRIMARY) > 0) {
       _blockBoundaries.push_back(i);
     }
   }

Dockerfile
@@ -17,7 +17,8 @@ RUN cmake -DCMAKE_BUILD_TYPE=Release -DLOGLEVEL=DEBUG -DUSE_PARALLEL=true .. && 
 
 FROM base as runtime
 WORKDIR /app
-RUN apt-get update && apt-get install -y wget python3-yaml unzip curl bzip2
+RUN apt-get update && apt-get install -y wget python3-yaml python3-pip unzip curl bzip2 pkg-config libicu-dev
+RUN pip3 install PyICU
 RUN apt-get update && apt-get install -y libgomp1 libicu-dev
 
 ARG UID=1000

e2e/e2e.sh
@@ -32,6 +32,8 @@ if [ -f "/usr/bin/python3.6" ]; then
 else
 	export PYTHON_BINARY=`which python3`
 fi
+export PYTHON_BINARY=`which python3`
+echo "Python is $PYTHON_BINARY"
 
 INDEX_DIR="$PROJECT_DIR/e2e_data"
 INPUT_DIR="$INDEX_DIR/scientist-collection"

e2e/queryit.py
@@ -10,6 +10,7 @@ from typing import Dict, Any, List
 from enum import Enum
 import json
 import yaml
+import icu
 
 class Color:
     """
@@ -97,6 +98,8 @@ def test_check(check_dict: Dict[str, Any], result: Dict[str, Any]) -> bool:
     """
     Test if the named result check holds. Returns True if it does
     """
+
+    collator = icu.Collator.createInstance(icu.Locale('de_DE.UTF-8'))
     res = result['res']
     for check, value in check_dict.items():
         if check == 'num_rows':
@@ -156,13 +159,13 @@ def test_check(check_dict: Dict[str, Any], result: Dict[str, Any]) -> bool:
                         previous_value = float(quotes_inner(previous))
                         current_value = float(quotes_inner(current))
                     elif col_type == 'string':
-                        previous_value = previous
-                        current_value = current
+                        previous_value = collator.getSortKey(previous)
+                        current_value = collator.getSortKey(current)
                     if direction.lower() == 'asc' and previous_value > current_value:
-                        eprint('order_numeric check failed:\n\tnot ascending')
+                        eprint('order_numeric check failed:\n\tnot ascending for {} and {}'.format(previous_value, current_value))
                         return False
                     if direction.lower() == 'desc' and previous_value < current_value:
-                        eprint('order_numeric check failed:\n\tnot descending')
+                        eprint('order_numeric check failed:\n\tnot ascending for {} and {}'.format(previous_value, current_value))
                         return False
             except ValueError as ex:
                 eprint('order_numeric check failed:\n\t' + str(ex))

src/engine/Filter.cpp
@@ -417,7 +417,7 @@ void Filter::computeFilterFixedValue(
         // remove the leading '^' symbol
         std::string rhs = _rhs.substr(1);
         // TODO<joka921>: handle Levels correctly;
-        auto [lowerBound, upperBound] = getIndex().getVocab().prefix_range(rhs, TripleComponentComparator::Level::PRIMARY);
+        auto [lowerBound, upperBound] = getIndex().getVocab().prefix_range(rhs);
 
         LOG(DEBUG) << "upper and lower bound are " << upperBound << ' '
                    << lowerBound << std::endl;
@@ -524,7 +524,6 @@ void Filter::computeResultFixedValue(
           // Remove the quotation marks surrounding the string.
           rhs_string = rhs_string.substr(1, rhs_string.size() - 2);
         }
-
       }
 
       // TODO<joka921> which level do we want for these filters

src/index/Index.cpp
@@ -95,8 +95,8 @@ void Index::createFromFile(const string& filename) {
     }
   }
   _configurationJson["prefixes"] = _vocabPrefixCompressed;
-  Vocabulary<CompressedString>::prefixCompressFile(vocabFile, vocabFileTmp,
-                                                   prefixes);
+  Vocabulary<CompressedString, TripleComponentComparator>::prefixCompressFile(
+      vocabFile, vocabFileTmp, prefixes);
 
   // TODO<joka921> maybe move this to its own function
   if (std::rename(vocabFileTmp.c_str(), vocabFile.c_str())) {
@@ -118,7 +118,6 @@ template void Index::createFromFile<TurtleMmapParser>(const string& filename);
 template <class Parser>
 VocabularyData Index::passFileForVocabulary(const string& filename,
                                             size_t linesPerPartial) {
-
   ParallelParseBuffer<Parser> p(PARSER_BATCH_SIZE, filename);
   size_t i = 0;
   // already count the numbers of triples that will be used for the language
@@ -184,8 +183,7 @@ VocabularyData Index::passFileForVocabulary(const string& filename,
         sortFutures.second.get();
       }
 
-      auto oldItemPtr = std::make_shared<const ItemMap>(
-          std::move(items));
+      auto oldItemPtr = std::make_shared<const ItemMap>(std::move(items));
       sortFutures = writeNextPartialVocabulary(
           i, numFiles, actualCurrentPartialSize, oldItemPtr);
       numFiles++;
@@ -208,8 +206,7 @@ VocabularyData Index::passFileForVocabulary(const string& filename,
     if (sortFutures.second.valid()) {
       sortFutures.second.get();
     }
-    auto oldItemPtr = std::make_shared<const ItemMap>(
-        std::move(items));
+    auto oldItemPtr = std::make_shared<const ItemMap>(std::move(items));
     sortFutures = writeNextPartialVocabulary(
         i, numFiles, actualCurrentPartialSize, oldItemPtr);
     numFiles++;
@@ -514,7 +511,7 @@ void Index::exchangeMultiplicities(MetaData* m1, MetaData* m2) {
 
 // _____________________________________________________________________________
 void Index::addPatternsToExistingIndex() {
-  auto [langPredLowerBound, langPredUpperBound] = _vocab.prefix_range("@", TripleComponentComparator::Level::IDENTICAL);
+  auto [langPredLowerBound, langPredUpperBound] = _vocab.prefix_range("@");
   createPatternsImpl<MetaDataIterator<IndexMetaDataMmapView>,
                      IndexMetaDataMmapView, ad_utility::File>(
       _onDiskBase + ".index.patterns", _hasPredicate, _hasPattern, _patterns,
@@ -1286,7 +1283,7 @@ template void Index::writeAsciiListFile<vector<Score>>(
 
 // _____________________________________________________________________________
 bool Index::isLiteral(const string& object) {
-  return Vocabulary<string>::isLiteral(object);
+  return decltype(_vocab)::isLiteral(object);
 }
 
 // _____________________________________________________________________________
@@ -1370,7 +1367,9 @@ void Index::readConfiguration() {
   }
 
   if (_configurationJson.count("ignore-case")) {
-    LOG(ERROR) << "Key \"ignore-case\" is no longer supported. This is an old index build that is no longer supported. Please rebuild your index using the \"locale\" key  \n";
+    LOG(ERROR) << "Key \"ignore-case\" is no longer supported. This is an old "
+                  "index build that is no longer supported. Please rebuild "
+                  "your index using the \"locale\" key  \n";
     throw std::runtime_error("Deprecated key \"ignore-case\" in index build");
   }
 
@@ -1381,8 +1380,11 @@ void Index::readConfiguration() {
     _vocab.setLocale(lang, country, ignorePunctuation);
     _textVocab.setLocale(lang, country, ignorePunctuation);
   } else {
-    LOG(ERROR) << "Key \"locale\" is missing in the metadata. This is probably and old index build that is no longer supported by QLever. Please rebuild your index\n";
-    throw std::runtime_error("Missing required key \"locale\" in index build's metadata");
+    LOG(ERROR) << "Key \"locale\" is missing in the metadata. This is probably "
+                  "and old index build that is no longer supported by QLever. "
+                  "Please rebuild your index\n";
+    throw std::runtime_error(
+        "Missing required key \"locale\" in index build's metadata");
   }
 
   if (_configurationJson.find("languages-internal") !=
@@ -1401,7 +1403,7 @@ string Index::tripleToInternalRepresentation(array<string, 3>* triplePtr) {
     spo[2] = ad_utility::convertValueLiteralToIndexWord(spo[2]);
     upperBound = 2;
   } else if (isLiteral(spo[2])) {
-    langtag = Vocabulary<string>::getLanguage(spo[2]);
+    langtag = decltype(_vocab)::getLanguage(spo[2]);
   }
 
   for (size_t k = 0; k < upperBound; ++k) {
@@ -1414,11 +1416,12 @@ string Index::tripleToInternalRepresentation(array<string, 3>* triplePtr) {
 
 // ___________________________________________________________________________
 void Index::initializeVocabularySettingsBuild() {
-  json j; // if we have no settings, we still have to initialize some default values
+  json j;  // if we have no settings, we still have to initialize some default
+           // values
   if (!_settingsFileName.empty()) {
-  std::ifstream f(_settingsFileName);
-  AD_CHECK(f.is_open());
-  f >> j;
+    std::ifstream f(_settingsFileName);
+    AD_CHECK(f.is_open());
+    f >> j;
   }
 
   if (j.find("prefixes-external") != j.end()) {
@@ -1429,10 +1432,11 @@ void Index::initializeVocabularySettingsBuild() {
   }
 
   if (j.count("ignore-case")) {
-    LOG(ERROR) << "Key \"ignore-case\" is no longer supported for the settings JSON. Please specify a \"locale\" instead (or remove the key for the default locale, see doc)\n";
+    LOG(ERROR) << "Key \"ignore-case\" is no longer supported for the settings "
+                  "JSON. Please specify a \"locale\" instead (or remove the "
+                  "key for the default locale, see doc)\n";
     throw std::runtime_error("Deprecated key \"ignore-case\" in settings JSON");
   }
-
 
   {
     std::string lang = "en";
@@ -1444,9 +1448,11 @@ void Index::initializeVocabularySettingsBuild() {
       country = j["locale"]["country"];
       ignorePunctuation = j["locale"]["ignore-punctuation"];
     } else {
-      LOG(INFO) << "locale was not specified by the settings JSON, defaulting to en US\n";
+      LOG(INFO) << "locale was not specified by the settings JSON, defaulting "
+                   "to en US\n";
     }
-    LOG(INFO) << "Using Locale " << lang << " " << country << " with ignore-punctuation: " << ignorePunctuation<< '\n';
+    LOG(INFO) << "Using Locale " << lang << " " << country
+              << " with ignore-punctuation: " << ignorePunctuation << '\n';
     _vocab.setLocale(lang, country, ignorePunctuation);
     _textVocab.setLocale(lang, country, ignorePunctuation);
     _configurationJson["locale"]["language"] = lang;
@@ -1467,7 +1473,9 @@ Id Index::assignNextId(Index::ItemMap* mapPtr, const string& key) {
   ItemMap& map = *mapPtr;
   if (!map.count(key)) {
     Id res = map.size();
-    map[key] = std::pair(map.size(), _vocab.getCaseComparator().extractAndTransformComparable(key, TripleComponentComparator::Level::IDENTICAL));
+    map[key] = std::pair(
+        map.size(), _vocab.getCaseComparator().extractAndTransformComparable(
+                        key, TripleComponentComparator::Level::IDENTICAL));
     return res;
   } else {
     return map[key].first;
@@ -1503,8 +1511,8 @@ pair<std::future<void>, std::future<void>> Index::writeNextPartialVocabulary(
               << partialTmpFilename << std::endl;
     LOG(INFO) << "it contains " << items->size() << " elements\n";
     fut2 = std::async([&items, partialTmpFilename]() {
-      writePartialIdMapToBinaryFileForMerging(
-          items, partialTmpFilename, SortMode::Simple);
+      writePartialIdMapToBinaryFileForMerging(items, partialTmpFilename,
+                                              SortMode::Simple);
     });
   }
   return {std::move(fut1), std::move(fut2)};

src/index/Index.h
@@ -444,17 +444,20 @@ class Index {
     LOG(DEBUG) << "Scan done, got " << result->size() << " elements.\n";
   }
 
-  using ItemMap = ad_utility::HashMap<string, std::pair<Id, TripleComponentComparator::SplitVal>>;
+  using ItemMap =
+      ad_utility::HashMap<string,
+                          std::pair<Id, TripleComponentComparator::SplitVal>>;
+
  private:
   string _onDiskBase;
   string _settingsFileName;
   bool _onDiskLiterals = false;
   bool _keepTempFiles = false;
   json _configurationJson;
-  Vocabulary<CompressedString> _vocab;
+  Vocabulary<CompressedString, TripleComponentComparator> _vocab;
   size_t _totalVocabularySize = 0;
   bool _vocabPrefixCompressed = true;
-  Vocabulary<std::string> _textVocab;
+  Vocabulary<std::string, SimpleStringComparator> _textVocab;
 
   TextMetaData _textMeta;
   DocsDB _docsDB;

src/index/MetaDataConverter.cpp
@@ -222,7 +222,7 @@ void CompressVocabAndCreateConfigurationFile(const string& indexPrefix) {
     auto prefixes =
         calculatePrefixes(vocabFilename, NUM_COMPRESSION_PREFIXES, 1);
     j["prefixes"] = prefixes;
-    Vocabulary<CompressedString>::prefixCompressFile(
+    Vocabulary<CompressedString, TripleComponentComparator>::prefixCompressFile(
         vocabFilename, vocabFilename + ".converted", prefixes);
     notifyCreated(vocabFilename, true);
     std::ofstream f(confFilename);

commit 27d98300693e14055f1364c55a28abfc9a0ac9d9

Fix block boundary bug introduced by Unicode refactoring
After 079b7080dade63094b61f696c56c3bca94f6e5af, the block boundary
computation was no longer correct: there was always one block per word.
I stumbled upon this when a freshly build index with text no longer
supported the * in the argument of ql:contains-word

It seems to me that there was simply one line missing, please double-check

TODO: add a unit text that checks that the block boundaries are not the
trivial boundaries (one block per word)

src/index/Index.Text.cpp
@@ -416,6 +416,7 @@ void Index::calculateBlockBoundaries() {
         locManager.compare(currentLenAndPrefix.second, nextLenAndPrefix.second,
                            LocaleManager::Level::PRIMARY) > 0) {
       _blockBoundaries.push_back(i);
+      currentLenAndPrefix = nextLenAndPrefix;
     }
   }
   _blockBoundaries.push_back(_textVocab.size() - 1);