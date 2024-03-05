qaecy/qlever

commit 9c3f234290c37a5538350ab914b9ec236d8ec50c

Store `col0Id` in permutations, smaller `.meta` files, improved full scan (#1289)
The `col0Id` is now stored in each permutation (one of the `.index.???` files) just like the `col1Id` and the `col2Id`. The `.meta` file for each permutation now only stores information for those `col0Id` that span multiple blocks. Despite the increased capabilities (see below), the relevant code is now simpler and easier to understand because `col0Id`, `col1Id`, and `col2Id` can now be treated in an analogous fashion.

This change reduces the size of the `.meta` files to a small fraction of their previous size (they used to be very large for permutations with many "relations", that is, for SPO and SOP with many subjects, and for OSP and OPS with many objects). In contrast, the size of the `.index.???` files inceases only little because they are compressed and due to their sort order contain long runs of triples with the same `col0Id`. In total, this change reduces the total size of the index files by half or even more.

In a closely related change, full index scans (queries involving patterns like `?s ?p ?o` and which need to read the full triples with `col0Id`, `col1Id`, `col2Id` from a permutation), can now be treated just like normal scans. This removes a lot of the extra code that accumulated over time for the special treatment of full scans. Also, these scans are now as efficient (per block) as a normal index scan. Another nice side effect is that full scans with GROUP BY and COUNT can now be processed with the code from #1263.

src/engine/Join.cpp
@@ -41,13 +41,6 @@ Join::Join(QueryExecutionContext* qec, std::shared_ptr<QueryExecutionTree> t1,
   if (t1->getCacheKey() > t2->getCacheKey()) {
     swapChildren();
   }
-  if (isFullScanDummy(t1)) {
-    AD_CONTRACT_CHECK(!isFullScanDummy(t2));
-    swapChildren();
-  } else if (t1->getType() == QueryExecutionTree::SCAN &&
-             t2->getType() != QueryExecutionTree::SCAN) {
-    swapChildren();
-  }
   _left = std::move(t1);
   _leftJoinCol = t1JoinCol;
   _right = std::move(t2);
@@ -98,12 +91,6 @@ ResultTable Join::computeResult() {
     _left->getRootOperation()->updateRuntimeInformationWhenOptimizedOut();
     _right->getRootOperation()->updateRuntimeInformationWhenOptimizedOut();
     return {std::move(idTable), resultSortedOn(), LocalVocab()};
-  }
-
-  // Check for joins with dummy
-  AD_CORRECTNESS_CHECK(!isFullScanDummy(_left));
-  if (isFullScanDummy(_right)) {
-    return computeResultForJoinWithFullScanDummy();
   }
 
   // Always materialize results that meet one of the following criteria:
@@ -197,10 +184,6 @@ ResultTable Join::computeResult() {
 
 // _____________________________________________________________________________
 VariableToColumnMap Join::computeVariableToColumnMap() const {
-  AD_CORRECTNESS_CHECK(!isFullScanDummy(_left));
-  if (isFullScanDummy(_right)) {
-    AD_CORRECTNESS_CHECK(_rightJoinCol == ColumnIndex{0});
-  }
   return makeVarToColMapForJoinOperation(
       _left->getVariableColumns(), _right->getVariableColumns(),
       {{_leftJoinCol, _rightJoinCol}}, BinOpType::Join,
@@ -216,17 +199,11 @@ size_t Join::getResultWidth() const {
 }
 
 // _____________________________________________________________________________
-vector<ColumnIndex> Join::resultSortedOn() const {
-  if (!isFullScanDummy(_left)) {
-    return {_leftJoinCol};
-  } else {
-    return {ColumnIndex{2 + _rightJoinCol}};
-  }
-}
+vector<ColumnIndex> Join::resultSortedOn() const { return {_leftJoinCol}; }
 
 // _____________________________________________________________________________
 float Join::getMultiplicity(size_t col) {
-  if (_multiplicities.size() == 0) {
+  if (_multiplicities.empty()) {
     computeSizeEstimateAndMultiplicities();
     _sizeEstimateComputed = true;
   }
@@ -235,119 +212,14 @@ float Join::getMultiplicity(size_t col) {
 
 // _____________________________________________________________________________
 size_t Join::getCostEstimate() {
-  size_t costJoin;
-
-  // The cost estimates of the "Join with full scan" case must be consistent
-  // with the estimates for the materialization of a full scan. For a detailed
-  // discussion see the comments in `IndexScan::getCostEstimate()` (in
-  // `IndexScan.cpp`)
-  if (isFullScanDummy(_left)) {
-    size_t nofDistinctTabJc = static_cast<size_t>(
-        _right->getSizeEstimate() / _right->getMultiplicity(_rightJoinCol));
-    float averageScanSize = _left->getMultiplicity(_leftJoinCol);
-
-    costJoin = nofDistinctTabJc * averageScanSize * 10'000;
-  } else if (isFullScanDummy(_right)) {
-    size_t nofDistinctTabJc = static_cast<size_t>(
-        _left->getSizeEstimate() / _left->getMultiplicity(_leftJoinCol));
-    float averageScanSize = _right->getMultiplicity(_rightJoinCol);
-    costJoin = nofDistinctTabJc * averageScanSize * 10'000;
-  } else {
-    // Normal case:
-    costJoin = _left->getSizeEstimate() + _right->getSizeEstimate();
-  }
+  size_t costJoin = _left->getSizeEstimate() + _right->getSizeEstimate();
 
   // TODO<joka921> once the `getCostEstimate` functions are `const`,
   // the argument can also be `const auto`
-  auto costIfNotFullScan = [](auto& subtree) {
-    return isFullScanDummy(subtree) ? size_t{0} : subtree->getCostEstimate();
-  };
+  auto costOfSubtree = [](auto& subtree) { return subtree->getCostEstimate(); };
 
-  return getSizeEstimateBeforeLimit() + costJoin + costIfNotFullScan(_left) +
-         costIfNotFullScan(_right);
-}
-
-// _____________________________________________________________________________
-ResultTable Join::computeResultForJoinWithFullScanDummy() {
-  IdTable idTable{getExecutionContext()->getAllocator()};
-  LOG(DEBUG) << "Join by making multiple scans..." << endl;
-  AD_CORRECTNESS_CHECK(!isFullScanDummy(_left) && isFullScanDummy(_right));
-  _right->getRootOperation()->updateRuntimeInformationWhenOptimizedOut(
-      {}, RuntimeInformation::Status::lazilyMaterialized);
-  idTable.setNumColumns(_left->getResultWidth() + 2);
-
-  shared_ptr<const ResultTable> nonDummyRes = _left->getResult();
-
-  doComputeJoinWithFullScanDummyRight(nonDummyRes->idTable(), &idTable);
-  LOG(DEBUG) << "Join (with dummy) done. Size: " << idTable.size() << endl;
-  checkCancellation();
-  return {std::move(idTable), resultSortedOn(),
-          nonDummyRes->getSharedLocalVocab()};
-}
-
-// _____________________________________________________________________________
-Join::ScanMethodType Join::getScanMethod(
-    std::shared_ptr<QueryExecutionTree> fullScanDummyTree) const {
-  ScanMethodType scanMethod;
-  IndexScan& scan =
-      *static_cast<IndexScan*>(fullScanDummyTree->getRootOperation().get());
-
-  // this works because the join operations execution Context never changes
-  // during its lifetime
-  const auto& idx = _executionContext->getIndex();
-  const auto scanLambda =
-      [&idx, &scan](const Permutation::Enum perm,
-                    ad_utility::SharedCancellationHandle cancellationHandle) {
-        return [&idx, perm, &scan,
-                cancellationHandle = std::move(cancellationHandle)](Id id) {
-          return idx.scan(id, std::nullopt, perm, scan.additionalColumns(),
-                          cancellationHandle);
-        };
-      };
-  AD_CORRECTNESS_CHECK(scan.getResultWidth() == 3);
-  return scanLambda(scan.permutation(), cancellationHandle_);
-}
-
-// _____________________________________________________________________________
-void Join::doComputeJoinWithFullScanDummyRight(const IdTable& ndr,
-                                               IdTable* res) const {
-  if (ndr.empty()) {
-    return;
-  }
-  // Get the scan method (depends on type of dummy tree), use a function ptr.
-  const ScanMethodType scan = getScanMethod(_right);
-  // Iterate through non-dummy.
-  Id currentJoinId = ndr(0, _leftJoinCol);
-  // TODO<C++23> This can be simplified using `std::views::chunk_by`.
-  auto joinItemFrom = ndr.begin();
-  auto joinItemEnd = ndr.begin();
-  for (size_t i = 0; i < ndr.size(); ++i) {
-    // For each different element in the join column.
-    if (ndr(i, _leftJoinCol) == currentJoinId) {
-      ++joinItemEnd;
-    } else {
-      // Do a scan.
-      LOG(TRACE) << "Inner scan with ID: " << currentJoinId << endl;
-      // The scan is a relatively expensive disk operation, so we can afford to
-      // check for timeouts before each call.
-      checkCancellation();
-      IdTable jr = scan(currentJoinId);
-      LOG(TRACE) << "Got #items: " << jr.size() << endl;
-      // Build the cross product.
-      appendCrossProduct(joinItemFrom, joinItemEnd, jr.cbegin(), jr.cend(),
-                         res);
-      // Reset
-      currentJoinId = ndr[i][_leftJoinCol];
-      joinItemFrom = joinItemEnd;
-      ++joinItemEnd;
-    }
-  }
-  // Do the scan for the final element.
-  LOG(TRACE) << "Inner scan with ID: " << currentJoinId << endl;
-  IdTable jr = scan(currentJoinId);
-  LOG(TRACE) << "Got #items: " << jr.size() << endl;
-  // Build the cross product.
-  appendCrossProduct(joinItemFrom, joinItemEnd, jr.cbegin(), jr.cend(), res);
+  return getSizeEstimateBeforeLimit() + costJoin + costOfSubtree(_left) +
+         costOfSubtree(_right);
 }
 
 // _____________________________________________________________________________
@@ -376,14 +248,10 @@ void Join::computeSizeEstimateAndMultiplicities() {
       _right->getSizeEstimate() *
       (static_cast<double>(nofDistinctInResult) / nofDistinctRight);
 
-  double corrFactor =
-      _executionContext
-          ? ((isFullScanDummy(_left) || isFullScanDummy(_right))
-                 ? _executionContext->getCostFactor(
-                       "DUMMY_JOIN_SIZE_ESTIMATE_CORRECTION_FACTOR")
-                 : _executionContext->getCostFactor(
-                       "JOIN_SIZE_ESTIMATE_CORRECTION_FACTOR"))
-          : 1;
+  double corrFactor = _executionContext
+                          ? _executionContext->getCostFactor(
+                                "JOIN_SIZE_ESTIMATE_CORRECTION_FACTOR")
+                          : 1.0;
 
   double jcMultiplicityInResult = _left->getMultiplicity(_leftJoinCol) *
                                   _right->getMultiplicity(_rightJoinCol);
@@ -395,8 +263,7 @@ void Join::computeSizeEstimateAndMultiplicities() {
              << " * " << jcMultiplicityInResult << " * " << nofDistinctInResult
              << std::endl;
 
-  for (auto i = isFullScanDummy(_left) ? ColumnIndex{1} : ColumnIndex{0};
-       i < _left->getResultWidth(); ++i) {
+  for (auto i = ColumnIndex{0}; i < _left->getResultWidth(); ++i) {
     double oldMult = _left->getMultiplicity(i);
     double m = std::max(
         1.0, oldMult * _right->getMultiplicity(_rightJoinCol) * corrFactor);
@@ -408,7 +275,7 @@ void Join::computeSizeEstimateAndMultiplicities() {
     _multiplicities.emplace_back(m);
   }
   for (auto i = ColumnIndex{0}; i < _right->getResultWidth(); ++i) {
-    if (i == _rightJoinCol && !isFullScanDummy(_left)) {
+    if (i == _rightJoinCol) {
       continue;
     }
     double oldMult = _right->getMultiplicity(i);
@@ -423,28 +290,6 @@ void Join::computeSizeEstimateAndMultiplicities() {
   }
 
   assert(_multiplicities.size() == getResultWidth());
-}
-
-// ______________________________________________________________________________
-void Join::appendCrossProduct(const IdTable::const_iterator& leftBegin,
-                              const IdTable::const_iterator& leftEnd,
-                              const IdTable::const_iterator& rightBegin,
-                              const IdTable::const_iterator& rightEnd,
-                              IdTable* res) const {
-  for (auto itl = leftBegin; itl != leftEnd; ++itl) {
-    for (auto itr = rightBegin; itr != rightEnd; ++itr) {
-      const auto& l = *itl;
-      const auto& r = *itr;
-      res->emplace_back();
-      size_t backIdx = res->size() - 1;
-      for (size_t i = 0; i < l.numColumns(); i++) {
-        (*res)(backIdx, i) = l[i];
-      }
-      for (size_t i = 0; i < r.numColumns(); i++) {
-        (*res)(backIdx, l.numColumns() + i) = r[i];
-      }
-    }
-  }
 }
 
 // ______________________________________________________________________________

test/HasPredicateScanTest.cpp
@@ -42,8 +42,8 @@ class HasPredicateScanTest : public ::testing::Test {
   // Expect that the result of the `operation` matches the `expectedElements`.
   void runTest(Operation& operation, const VectorTable& expectedElements) {
     auto expected = makeIdTableFromVector(expectedElements);
-    EXPECT_THAT(operation.getResult()->idTable(),
-                ::testing::ElementsAreArray(expected));
+    auto res = operation.getResult();
+    EXPECT_THAT(res->idTable(), ::testing::ElementsAreArray(expected));
   }
 
   // Expect that the result of the `operation` matches the `expectedElements`,
@@ -111,7 +111,7 @@ TEST_F(HasPredicateScanTest, subtree) {
   auto indexScan = ad_utility::makeExecutionTree<IndexScan>(
       qec, Permutation::Enum::OPS, SparqlTriple{V{"?x"}, "?y", "<o4>"});
   auto scan = HasPredicateScan{qec, indexScan, 1, V{"?predicate"}};
-  runTest(scan, {{p3, y, p}, {p3, y, p3}});
+  runTest(scan, {{y, p, p3}, {y, p3, p3}});
 }
 
 // ____________________________________________________________

src/engine/CountAvailablePredicates.cpp
@@ -163,7 +163,8 @@ void CountAvailablePredicates::computePatternTrickAllEntities(
           ->getIndex()
           .getImpl()
           .getPermutation(Permutation::Enum::PSO)
-          .lazyScan(qlever::specialIds.at(HAS_PATTERN_PREDICATE), std::nullopt,
+          .lazyScan({qlever::specialIds.at(HAS_PATTERN_PREDICATE), std::nullopt,
+                     std::nullopt},
                     std::nullopt, {}, cancellationHandle_);
   for (const auto& idTable : fullHasPattern) {
     for (const auto& patternId : idTable.getColumn(1)) {

src/engine/GroupBy.cpp
@@ -438,9 +438,9 @@ bool GroupBy::computeGroupByForSingleIndexScan(IdTable* result) {
           getPermutationForThreeVariableTriple(*_subtree, var, var);
       AD_CONTRACT_CHECK(permutation.has_value());
       table(0, 0) = Id::makeFromInt(
-          getIndex().getImpl().numDistinctCol0(permutation.value()).normal_);
+          getIndex().getImpl().numDistinctCol0(permutation.value()).normal);
     } else {
-      table(0, 0) = Id::makeFromInt(getIndex().numTriples().normal_);
+      table(0, 0) = Id::makeFromInt(getIndex().numTriples().normal);
     }
   } else {
     // TODO<joka921> The two variables IndexScans should also account for the
@@ -555,47 +555,19 @@ bool GroupBy::computeGroupByForFullIndexScan(IdTable* result) {
     const auto& permutation =
         getExecutionContext()->getIndex().getPimpl().getPermutation(
             permutationEnum.value());
-    IdTableStatic<NUM_COLS> table = std::move(*idTable).toStatic<NUM_COLS>();
-    const auto& metaData = permutation.meta_.data();
-    // TODO<joka921> the reserve is too large because of the ignored
-    // triples. We would need to incorporate the information how many
-    // added "relations" are in each permutationEnum during index building.
-    table.reserve(metaData.size());
-    for (auto it = metaData.ordered_begin(); it != metaData.ordered_end();
-         ++it) {
-      Id id = decltype(metaData.ordered_begin())::getIdFromElement(*it);
-
-      // Check whether this is an `@en@...` predicate in a `Pxx`
-      // permutationEnum, a literal in a `Sxx` permutationEnum or some other
-      // entity that was added only for internal reasons.
-      if (std::ranges::any_of(ignoredRanges, [&id](const auto& pair) {
-            return id >= pair.first && id < pair.second;
-          })) {
-        continue;
-      }
-      Id count = Id::makeFromInt(
-          decltype(metaData.ordered_begin())::getNumRowsFromElement(*it));
-      // TODO<joka921> The count is actually not accurate at least for the
-      // `Sxx` and `Oxx` permutations because it contains the triples with
-      // predicate
-      // `@en@rdfs:label` etc. The probably easiest way to fix this is to
-      // exclude these triples from those permutations (they are only
-      // relevant for queries with a fixed subject), but then we would
-      // need to make sure, that we don't accidentally break the language
-      // filters for queries like
-      // `<fixedSubject> @en@rdfs:label ?labels`, for which the best
-      // query plan potentially goes through the `SPO` relation.
-      // Alternatively we would have to write an additional number
-      // `numNonAddedTriples` to the `IndexMetaData` which would further
-      // increase their size.
-      // TODO<joka921> Discuss this with Hannah.
-      table.emplace_back();
-      table(table.size() - 1, 0) = id;
-      if (numCounts == 1) {
-        table(table.size() - 1, 1) = count;
-      }
+    auto table = permutation.getDistinctCol0IdsAndCounts(cancellationHandle_);
+    if (NUM_COLS == 1) {
+      table.setColumnSubset({{0}});
     }
-    *idTable = std::move(table).toDynamic();
+    // TODO<joka921> This is only semi-efficient.
+    auto end = std::ranges::remove_if(table, [&ignoredRanges](const auto& row) {
+      return std::ranges::any_of(ignoredRanges,
+                                 [id = row[0]](const auto& pair) {
+                                   return id >= pair.first && id < pair.second;
+                                 });
+    });
+    table.resize(end.begin() - table.begin());
+    *idTable = std::move(table);
   };
   ad_utility::callFixedSize(numCols, doComputationForNumberOfColumns, result);
 

src/engine/HasPredicateScan.cpp
@@ -266,13 +266,13 @@ ResultTable HasPredicateScan::computeResult() {
   idTable.setNumColumns(getResultWidth());
 
   const CompactVectorOfStrings<Id>& patterns = getIndex().getPatterns();
-  auto hasPattern =
-      getExecutionContext()
-          ->getIndex()
-          .getImpl()
-          .getPermutation(Permutation::Enum::PSO)
-          .lazyScan(qlever::specialIds.at(HAS_PATTERN_PREDICATE), std::nullopt,
-                    std::nullopt, {}, cancellationHandle_);
+  auto hasPattern = getExecutionContext()
+                        ->getIndex()
+                        .getImpl()
+                        .getPermutation(Permutation::Enum::PSO)
+                        .lazyScan({qlever::specialIds.at(HAS_PATTERN_PREDICATE),
+                                   std::nullopt, std::nullopt},
+                                  std::nullopt, {}, cancellationHandle_);
 
   auto getId = [this](const TripleComponent tc) {
     std::optional<Id> id = tc.toValueId(getIndex().getVocab());
@@ -340,8 +340,9 @@ void HasPredicateScan::computeFreeO(
                         ->getIndex()
                         .getImpl()
                         .getPermutation(Permutation::Enum::PSO)
-                        .scan(qlever::specialIds.at(HAS_PATTERN_PREDICATE),
-                              subjectAsId, {}, cancellationHandle_);
+                        .scan({qlever::specialIds.at(HAS_PATTERN_PREDICATE),
+                               subjectAsId, std::nullopt},
+                              {}, cancellationHandle_);
   AD_CORRECTNESS_CHECK(hasPattern.numRows() <= 1);
   for (Id patternId : hasPattern.getColumn(0)) {
     const auto& pattern = patterns[patternId.getInt()];

commit 7e908c2e703db5b9ef5a265d02a1085cc43fbde4

Fix two bugs introduced by #1289 in the last but one commit (#1293)

src/engine/Join.cpp
@@ -32,13 +32,21 @@ Join::Join(QueryExecutionContext* qec, std::shared_ptr<QueryExecutionTree> t1,
   t1 = QueryExecutionTree::createSortedTree(std::move(t1), {t1JoinCol});
   t2 = QueryExecutionTree::createSortedTree(std::move(t2), {t2JoinCol});
 
-  // Make sure that the subtrees are ordered so that identical queries can be
-  // identified.
+  // Make the order of the two subtrees deterministic. That way, queries that
+  // are identical except for the order of the join operands, are easier to
+  // identify.
   auto swapChildren = [&]() {
     std::swap(t1, t2);
     std::swap(t1JoinCol, t2JoinCol);
   };
   if (t1->getCacheKey() > t2->getCacheKey()) {
+    swapChildren();
+  }
+  // If one of the inputs is a SCAN and the other one is not, always make the
+  // SCAN the right child (which also gives a deterministic order of the
+  // subtrees). This simplifies several branches in the `computeResult` method.
+  if (t1->getType() == QueryExecutionTree::SCAN &&
+      t2->getType() != QueryExecutionTree::SCAN) {
     swapChildren();
   }
   _left = std::move(t1);

src/engine/RuntimeInformation.cpp
@@ -98,21 +98,38 @@ void RuntimeInformation::writeToStream(std::ostream& out, size_t indent) const {
 
 // __________________________________________________________________________
 void RuntimeInformation::setColumnNames(const VariableToColumnMap& columnMap) {
+  // Empty hash map -> empty vector.
   if (columnMap.empty()) {
     columnNames_.clear();
     return;
   }
 
+  // Resize the `columnNames_` vector such that we can use the keys from
+  // columnMap (which are not necessarily consecutive) as indexes.
   ColumnIndex maxColumnIndex = std::ranges::max(
       columnMap | std::views::values |
       std::views::transform(&ColumnIndexAndTypeInfo::columnIndex_));
-  // Resize the `columnNames_` vector such that we can use the keys from
-  // columnMap (which are not necessarily consecutive) as indexes.
   columnNames_.resize(maxColumnIndex + 1);
-  // Now copy the (variable, index) pairs to the vector.
+
+  // Now copy the `variable, index` pairs from the map to the vector. If the
+  // column might contain UNDEF values, append ` (U)` to the variable name.
+  //
   for (const auto& [variable, columnIndexAndType] : columnMap) {
-    ColumnIndex columnIndex = columnIndexAndType.columnIndex_;
-    columnNames_.at(columnIndex) = variable.name();
+    const auto& [columnIndex, undefStatus] = columnIndexAndType;
+    std::string_view undefStatusSuffix =
+        undefStatus == ColumnIndexAndTypeInfo::UndefStatus::AlwaysDefined
+            ? ""
+            : " (U)";
+    columnNames_.at(columnIndex) =
+        absl::StrCat(variable.name(), undefStatusSuffix);
+  }
+  // Replace the empty column names (columns that are present in the result, but
+  // are not visible using a variable) by the placeholder "_" to make the
+  // runtime information more readable.
+  for (auto& name : columnNames_) {
+    if (name.empty()) {
+      name = "_";
+    }
   }
 }
 

src/engine/RuntimeInformation.h
@@ -93,8 +93,9 @@ class RuntimeInformation {
   /// library to allow for implicit conversion.
   friend void to_json(nlohmann::ordered_json& j, const RuntimeInformation& rti);
 
-  /// Set the names of the columns from the HashMap format that is used in the
-  /// rest of the Qlever code.
+  /// Set `columnNames_` from a `VariableToColumnMap`. The former is a vector
+  /// (convenient for this class), the latter is a hash map (appropriate for
+  /// the rest of the code).
   void setColumnNames(const VariableToColumnMap& columnMap);
 
   /// Get the time spent computing the operation. This is the total time minus

src/index/CompressedRelation.cpp
@@ -530,54 +530,66 @@ IdTable CompressedRelationReader::getDistinctColIdsAndCountsImpl(
     const ScanSpecification& scanSpec,
     const std::vector<CompressedBlockMetadata>& allBlocksMetadata,
     const CancellationHandle& cancellationHandle) const {
+  // The result has two columns: one for the distinct `Id`s and one for their
+  // counts.
   IdTableStatic<2> table(allocator_);
-  std::span<const CompressedBlockMetadata> relationBlocksMetadata =
-      getRelevantBlocks(scanSpec, allBlocksMetadata);
 
-  // Iterate over the blocks and only read (and decompress) those which
-  // contain more than one different `col1Id`. For the others, we can determine
-  // the count from the metadata.
-  std::optional<Id> currentCol1Id;
+  // The current `colId` and its current count.
+  std::optional<Id> currentColId;
   size_t currentCount = 0;
-  // Helper lambda that processes a single `col1Id` (with `howMany`
-  // occurrences). If it's new, a row with the previous `col1Id` and
-  // `currentCount` is added to `table`, and `currentCol1Id` and `currentCount`
-  // are reset.
-  auto processCol1Id = [&table, &currentCol1Id, &currentCount](
-                           std::optional<Id> col1Id, size_t howMany) {
-    if (col1Id != currentCol1Id) {
-      if (currentCol1Id.has_value()) {
-        table.push_back({currentCol1Id.value(), Id::makeFromInt(currentCount)});
+
+  // Helper lambda that processes the next `colId` and a count. If it's new, a
+  // row with the previous `currentColId` and its count are added to the
+  // result, and `currentColId` and its count are updated to the new `colId`.
+  auto processColId = [&table, &currentColId, &currentCount](
+                          std::optional<Id> colId, size_t colIdCount) {
+    if (colId != currentColId) {
+      if (currentColId.has_value()) {
+        table.push_back({currentColId.value(), Id::makeFromInt(currentCount)});
       }
-      currentCol1Id = col1Id;
+      currentColId = colId;
       currentCount = 0;
     }
-    currentCount += howMany;
+    currentCount += colIdCount;
   };
-  for (size_t i = 0; i < relationBlocksMetadata.size(); ++i) {
-    const auto& blockMetadata = relationBlocksMetadata[i];
-    Id firstCol1Id = std::invoke(idGetter, blockMetadata.firstTriple_);
-    Id lastCol1Id = std::invoke(idGetter, blockMetadata.lastTriple_);
-    if (firstCol1Id == lastCol1Id) {
-      // The whole block has the same `col1Id`.
-      processCol1Id(firstCol1Id, blockMetadata.numRows_);
+
+  // Get the blocks needed for the scan.
+  std::span<const CompressedBlockMetadata> relevantBlocksMetadata =
+      getRelevantBlocks(scanSpec, allBlocksMetadata);
+
+  // Get the index of the column with the `colId`s that we are interested in.
+  auto relevantColumnIndex = prepareColumnIndices(scanSpec, {});
+  AD_CORRECTNESS_CHECK(!relevantColumnIndex.empty());
+  relevantColumnIndex.resize(1);
+
+  // Iterate over the blocks and only read (and decompress) those which contain
+  // more than one different `colId`. For the others, we can determine the
+  // count from the metadata.
+  for (size_t i = 0; i < relevantBlocksMetadata.size(); ++i) {
+    const auto& blockMetadata = relevantBlocksMetadata[i];
+    Id firstColId = std::invoke(idGetter, blockMetadata.firstTriple_);
+    Id lastColId = std::invoke(idGetter, blockMetadata.lastTriple_);
+    if (firstColId == lastColId) {
+      // The whole block has the same `colId` -> we get all the information
+      // from the metadata.
+      processColId(firstColId, blockMetadata.numRows_);
     } else {
-      // Multiple `col1Id`s in one block.
-      std::array<ColumnIndex, 1> columnIndices{1u};
+      // Multiple `colId`s -> we have to read the block.
       const auto& block =
-          i == 0 ? readPossiblyIncompleteBlock(scanSpec, blockMetadata,
-                                               std::nullopt, columnIndices)
-                 : readAndDecompressBlock(blockMetadata, columnIndices);
+          i == 0
+              ? readPossiblyIncompleteBlock(scanSpec, blockMetadata,
+                                            std::nullopt, relevantColumnIndex)
+              : readAndDecompressBlock(blockMetadata, relevantColumnIndex);
       cancellationHandle->throwIfCancelled();
       // TODO<C++23>: use `std::views::chunk_by`.
       for (size_t j = 0; j < block.numRows(); ++j) {
-        Id col1Id = block(j, 0);
-        processCol1Id(col1Id, 1);
+        Id colId = block(j, 0);
+        processColId(colId, 1);
       }
     }
   }
   // Don't forget to add the last `col1Id` and its count.
-  processCol1Id(std::nullopt, 0);
+  processColId(std::nullopt, 0);
   return std::move(table).toDynamic();
 }
 

test/HasPredicateScanTest.cpp
@@ -111,7 +111,7 @@ TEST_F(HasPredicateScanTest, subtree) {
   auto indexScan = ad_utility::makeExecutionTree<IndexScan>(
       qec, Permutation::Enum::OPS, SparqlTriple{V{"?x"}, "?y", "<o4>"});
   auto scan = HasPredicateScan{qec, indexScan, 1, V{"?predicate"}};
-  runTest(scan, {{y, p, p3}, {y, p3, p3}});
+  runTest(scan, {{p3, y, p}, {p3, y, p3}});
 }
 
 // ____________________________________________________________