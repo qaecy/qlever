qaecy/qlever

commit 28e5db40c8c009d9623cd0bfe25a68a665a2d6ef

Add BlankNode support for SERVICE (#1504)
With this commit, QLever supports to add new blank nodes during the evaluation of a query. This function is used to support blank nodes in the result of a `SERVICE` request. These blank nodes are distinct from all blank nodes in the index, and also from all blank nodes from other SERVICE request, eve if they came from the same server. This behavior is correct accordin to the SPARQL 1.1 federated query standard.

src/engine/LocalVocab.cpp
@@ -77,3 +77,14 @@ std::vector<LocalVocab::LiteralOrIri> LocalVocab::getAllWordsForTesting()
   }
   return result;
 }
+
+// _____________________________________________________________________________
+BlankNodeIndex LocalVocab::getBlankNodeIndex(
+    ad_utility::BlankNodeManager* blankNodeManager) {
+  AD_CONTRACT_CHECK(blankNodeManager);
+  // Initialize the `localBlankNodeManager_` if it doesn't exist yet.
+  if (!localBlankNodeManager_) [[unlikely]] {
+    localBlankNodeManager_.emplace(blankNodeManager);
+  }
+  return BlankNodeIndex::make(localBlankNodeManager_->getId());
+}

src/engine/LocalVocab.h
@@ -13,6 +13,7 @@
 #include "absl/container/node_hash_set.h"
 #include "global/Id.h"
 #include "parser/LiteralOrIri.h"
+#include "util/BlankNodeManager.h"
 
 // A class for maintaining a local vocabulary with contiguous (local) IDs. This
 // is meant for words that are not part of the normal vocabulary (constructed
@@ -37,6 +38,9 @@ class LocalVocab {
 
   auto& primaryWordSet() { return *primaryWordSet_; }
   const auto& primaryWordSet() const { return *primaryWordSet_; }
+
+  std::optional<ad_utility::BlankNodeManager::LocalBlankNodeManager>
+      localBlankNodeManager_;
 
  public:
   // Create a new, empty local vocabulary.
@@ -89,6 +93,10 @@ class LocalVocab {
 
   // Return all the words from all the word sets as a vector.
   std::vector<LiteralOrIri> getAllWordsForTesting() const;
+
+  // Get a new BlankNodeIndex using the LocalBlankNodeManager.
+  [[nodiscard]] BlankNodeIndex getBlankNodeIndex(
+      ad_utility::BlankNodeManager* blankNodeManager);
 
  private:
   // Common implementation for the two variants of

src/util/BlankNodeManager.cpp
@@ -1,0 +1,62 @@
+// Copyright 2024, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Moritz Dom (domm@informatik.uni-freiburg.de)
+
+#include "util/BlankNodeManager.h"
+
+namespace ad_utility {
+
+// _____________________________________________________________________________
+BlankNodeManager::BlankNodeManager(uint64_t minIndex)
+    : minIndex_(minIndex),
+      randBlockIndex_(
+          SlowRandomIntGenerator<uint64_t>(0, totalAvailableBlocks_ - 1)) {}
+
+// _____________________________________________________________________________
+BlankNodeManager::Block BlankNodeManager::allocateBlock() {
+  // The Random-Generation Algorithm's performance is reduced once the number of
+  // used blocks exceeds a limit.
+  auto numBlocks = usedBlocksSet_.rlock()->size();
+  AD_CORRECTNESS_CHECK(
+      numBlocks < totalAvailableBlocks_ / 256,
+      absl::StrCat("Critical high number of blank node blocks in use: ",
+                   numBlocks, " blocks"));
+
+  auto usedBlocksSetPtr = usedBlocksSet_.wlock();
+  while (true) {
+    auto blockIdx = randBlockIndex_();
+    if (!usedBlocksSetPtr->contains(blockIdx)) {
+      usedBlocksSetPtr->insert(blockIdx);
+      return Block(blockIdx, minIndex_ + blockIdx * blockSize_);
+    }
+  }
+}
+
+// _____________________________________________________________________________
+BlankNodeManager::Block::Block(uint64_t blockIndex, uint64_t startIndex)
+    : blockIdx_(blockIndex), nextIdx_(startIndex) {}
+
+// _____________________________________________________________________________
+BlankNodeManager::LocalBlankNodeManager::LocalBlankNodeManager(
+    BlankNodeManager* blankNodeManager)
+    : blankNodeManager_(blankNodeManager) {}
+
+// _____________________________________________________________________________
+BlankNodeManager::LocalBlankNodeManager::~LocalBlankNodeManager() {
+  auto ptr = blankNodeManager_->usedBlocksSet_.wlock();
+  for (auto block : blocks_) {
+    AD_CONTRACT_CHECK(ptr->contains(block.blockIdx_));
+    ptr->erase(block.blockIdx_);
+  }
+}
+
+// _____________________________________________________________________________
+uint64_t BlankNodeManager::LocalBlankNodeManager::getId() {
+  if (blocks_.empty() || blocks_.back().nextIdx_ == idxAfterCurrentBlock_) {
+    blocks_.emplace_back(blankNodeManager_->allocateBlock());
+    idxAfterCurrentBlock_ = blocks_.back().nextIdx_ + blockSize_;
+  }
+  return blocks_.back().nextIdx_++;
+}
+
+}  // namespace ad_utility

src/util/BlankNodeManager.h
@@ -1,0 +1,103 @@
+// Copyright 2024, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Moritz Dom (domm@informatik.uni-freiburg.de)
+
+#pragma once
+
+#include <gtest/gtest_prod.h>
+
+#include <vector>
+
+#include "global/ValueId.h"
+#include "util/HashSet.h"
+#include "util/Random.h"
+#include "util/Synchronized.h"
+
+namespace ad_utility {
+/*
+ * Manager class owned by an `Index` to manage currently available indices for
+ * blank nodes to be added during runtime. The intention is to use the same
+ * `BlankNodeIndex`-Datatype as for blank nodes given at indexing time, by
+ * setting their count as the minimum index for the ones added at runtime.
+ * A `LocalVocab` can register new blank nodes (e.g. resulting from a `Service`
+ * operation) by obtaining a `Block` of currently unused indices using it's own
+ * `LocalBlankNodeManager` from the `BlankNodeManager`.
+ */
+class BlankNodeManager {
+ public:
+  // Minimum blank node index.
+  const uint64_t minIndex_;
+
+  // Number of indices that make up a single block.
+  static constexpr uint blockSize_ = 1000;
+
+  // Number of blocks available.
+  const uint64_t totalAvailableBlocks_ =
+      (ValueId::maxIndex - minIndex_ + 1) / blockSize_;
+
+ private:
+  // Int Generator yielding random block indices.
+  SlowRandomIntGenerator<uint64_t> randBlockIndex_;
+
+  // Tracks blocks currently used by instances of `LocalBlankNodeManager`.
+  Synchronized<HashSet<uint64_t>> usedBlocksSet_;
+
+ public:
+  // Constructor, where `minIndex` is the minimum index such that all managed
+  // indices are in [`minIndex_`, `ValueId::maxIndex`]. `minIndex_` is
+  // determined by the number of BlankNodes in the current Index.
+  explicit BlankNodeManager(uint64_t minIndex = 0);
+  ~BlankNodeManager() = default;
+
+  // A BlankNodeIndex Block of size `blockSize_`.
+  class Block {
+    // Intentional private constructor, allowing only the BlankNodeManager to
+    // create Blocks (for a `LocalBlankNodeManager`).
+    explicit Block(uint64_t blockIndex, uint64_t startIndex);
+    friend class BlankNodeManager;
+
+   public:
+    ~Block() = default;
+    // The index of this block.
+    const uint64_t blockIdx_;
+    // The next free index within this block.
+    uint64_t nextIdx_;
+  };
+
+  // Manages the BlankNodes used within a LocalVocab.
+  class LocalBlankNodeManager {
+   public:
+    explicit LocalBlankNodeManager(BlankNodeManager* blankNodeManager);
+    ~LocalBlankNodeManager();
+
+    // No copy, as the managed blocks shall not be duplicated.
+    LocalBlankNodeManager(const LocalBlankNodeManager&) = delete;
+    LocalBlankNodeManager& operator=(const LocalBlankNodeManager&) = delete;
+
+    LocalBlankNodeManager(LocalBlankNodeManager&&) = default;
+    LocalBlankNodeManager& operator=(LocalBlankNodeManager&&) = default;
+
+    // Get a new id.
+    [[nodiscard]] uint64_t getId();
+
+   private:
+    // Reserved blocks.
+    std::vector<BlankNodeManager::Block> blocks_;
+
+    // Reference of the BlankNodeManager, used to free the reserved blocks.
+    BlankNodeManager* blankNodeManager_;
+
+    // The first index after the current Block.
+    uint64_t idxAfterCurrentBlock_{0};
+
+    FRIEND_TEST(BlankNodeManager, LocalBlankNodeManagerGetID);
+  };
+
+  // Allocate and retrieve a block of free ids.
+  [[nodiscard]] Block allocateBlock();
+
+  FRIEND_TEST(BlankNodeManager, blockAllocationAndFree);
+  FRIEND_TEST(BlankNodeManager, moveLocalBlankNodeManager);
+};
+
+}  // namespace ad_utility

test/BlankNodeManagerTest.cpp
@@ -1,0 +1,82 @@
+// Copyright 2024, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Moritz Dom (domm@informatik.uni-freiburg.de)
+
+#include <gtest/gtest.h>
+
+#include "gmock/gmock.h"
+#include "util/BlankNodeManager.h"
+#include "util/GTestHelpers.h"
+
+namespace ad_utility {
+// _____________________________________________________________________________
+TEST(BlankNodeManager, blockAllocationAndFree) {
+  BlankNodeManager bnm(0);
+  EXPECT_TRUE(bnm.usedBlocksSet_.rlock()->empty());
+
+  {
+    // LocalBlankNodeManager allocates a new block
+    BlankNodeManager::LocalBlankNodeManager lbnm(&bnm);
+    [[maybe_unused]] uint64_t id = lbnm.getId();
+    EXPECT_EQ(bnm.usedBlocksSet_.rlock()->size(), 1);
+  }
+
+  // Once the LocalBlankNodeManager is destroyed, all Blocks allocated through
+  // it are freed/removed from the BlankNodeManager's set.
+  EXPECT_TRUE(bnm.usedBlocksSet_.rlock()->empty());
+
+  // Mock randomIntGenerator to let the block index generation collide.
+  bnm.randBlockIndex_ = SlowRandomIntGenerator<uint64_t>(0, 1);
+  [[maybe_unused]] auto _ = bnm.allocateBlock();
+  for (int i = 0; i < 30; ++i) {
+    auto block = bnm.allocateBlock();
+    bnm.usedBlocksSet_.wlock()->erase(block.blockIdx_);
+  }
+}
+
+// _____________________________________________________________________________
+TEST(BlankNodeManager, LocalBlankNodeManagerGetID) {
+  BlankNodeManager bnm(0);
+  BlankNodeManager::LocalBlankNodeManager l(&bnm);
+
+  // initially the LocalBlankNodeManager doesn't have any blocks
+  EXPECT_EQ(l.blocks_.size(), 0);
+
+  // A new Block is allocated, if
+  // no blocks are allocated yet
+  uint64_t id = l.getId();
+  EXPECT_EQ(l.blocks_.size(), 1);
+
+  // or the ids of the last block are all used
+  l.blocks_.back().nextIdx_ = id + BlankNodeManager::blockSize_;
+  id = l.getId();
+  EXPECT_EQ(l.blocks_.size(), 2);
+}
+
+// _____________________________________________________________________________
+TEST(BlankNodeManager, maxNumOfBlocks) {
+  // Mock a high `minIndex_` to simulate reduced space in the `usedBlocksSet_`
+  BlankNodeManager bnm(ValueId::maxIndex - 256 * BlankNodeManager::blockSize_ +
+                       2);
+  AD_EXPECT_THROW_WITH_MESSAGE(
+      [[maybe_unused]] auto _ = bnm.allocateBlock(),
+      ::testing::HasSubstr(
+          "Critical high number of blank node blocks in use:"));
+}
+
+// _____________________________________________________________________________
+TEST(BlankNodeManager, moveLocalBlankNodeManager) {
+  // This ensures that the `blocks_` of the `LocalBlankNodeManager` are moved
+  // correctly, such that they're freed/removed from the `BlankNodeManager`
+  // set only once.
+  BlankNodeManager bnm(0);
+  EXPECT_NO_THROW({
+    BlankNodeManager::LocalBlankNodeManager l1(&bnm);
+    auto l2(std::move(l1));
+    BlankNodeManager::LocalBlankNodeManager l3(&bnm);
+    l3 = std::move(l2);
+  });
+  EXPECT_TRUE(bnm.usedBlocksSet_.rlock()->empty());
+}
+
+}  // namespace ad_utility

test/LocalVocabTest.cpp
@@ -369,3 +369,12 @@ TEST(LocalVocab, propagation) {
   // TODO<joka921> Maybe add tests for the new TextIndexScanFor... classes,
   // they never introduce any local vocab.
 }
+
+// _____________________________________________________________________________
+TEST(LocalVocab, getBlankNodeIndex) {
+  ad_utility::BlankNodeManager bnm(0);
+  LocalVocab v;
+  BlankNodeIndex a = v.getBlankNodeIndex(&bnm);
+  BlankNodeIndex b = v.getBlankNodeIndex(&bnm);
+  EXPECT_NE(a, b);
+}

src/engine/QueryExecutionContext.h
@@ -7,7 +7,6 @@
 #pragma once
 
 #include <memory>
-#include <shared_mutex>
 #include <string>
 
 #include "engine/QueryPlanningCostFactors.h"

src/engine/Service.cpp
@@ -15,6 +15,7 @@
 #include "parser/RdfParser.h"
 #include "parser/TokenizerCtre.h"
 #include "util/Exception.h"
+#include "util/HashMap.h"
 #include "util/HashSet.h"
 #include "util/StringUtils.h"
 #include "util/http/HttpUtils.h"
@@ -204,6 +205,9 @@ void Service::writeJsonResult(const std::vector<std::string>& vars,
   IdTableStatic<I> idTable = std::move(*idTablePtr).toStatic<I>();
   checkCancellation();
   std::vector<size_t> numLocalVocabPerColumn(idTable.numColumns());
+  // TODO<joka921> We should include a memory limit, as soon as we can do proper
+  // memory-limited HashMaps.
+  ad_utility::HashMap<std::string, Id> blankNodeMap;
 
   auto writeBindings = [&](const nlohmann::json& bindings, size_t& rowIdx) {
     for (const auto& binding : bindings) {
@@ -211,7 +215,8 @@ void Service::writeJsonResult(const std::vector<std::string>& vars,
       for (size_t colIdx = 0; colIdx < vars.size(); ++colIdx) {
         TripleComponent tc =
             binding.contains(vars[colIdx])
-                ? bindingToTripleComponent(binding[vars[colIdx]])
+                ? bindingToTripleComponent(binding[vars[colIdx]], blankNodeMap,
+                                           localVocab)
                 : TripleComponent::UNDEF();
 
         Id id = std::move(tc).toValueId(getIndex().getVocab(), *localVocab);
@@ -359,7 +364,9 @@ std::optional<std::string> Service::getSiblingValuesClause() const {
 
 // ____________________________________________________________________________
 TripleComponent Service::bindingToTripleComponent(
-    const nlohmann::json& binding) {
+    const nlohmann::json& binding,
+    ad_utility::HashMap<std::string, Id>& blankNodeMap,
+    LocalVocab* localVocab) const {
   if (!binding.contains("type") || !binding.contains("value")) {
     throw std::runtime_error(absl::StrCat(
         "Missing type or value field in binding. The binding is: '",
@@ -368,6 +375,8 @@ TripleComponent Service::bindingToTripleComponent(
 
   const auto type = binding["type"].get<std::string_view>();
   const auto value = binding["value"].get<std::string_view>();
+  auto blankNodeManagerPtr =
+      getExecutionContext()->getIndex().getBlankNodeManager();
 
   TripleComponent tc;
   if (type == "literal") {
@@ -386,12 +395,12 @@ TripleComponent Service::bindingToTripleComponent(
   } else if (type == "uri") {
     tc = TripleComponent::Iri::fromIrirefWithoutBrackets(value);
   } else if (type == "bnode") {
-    throw std::runtime_error(
-        "Blank nodes in the result of a SERVICE are currently not "
-        "supported. "
-        "For now, consider filtering them out using the ISBLANK function "
-        "or "
-        "converting them via the STR function.");
+    auto [it, wasNew] = blankNodeMap.try_emplace(value, Id());
+    if (wasNew) {
+      it->second = Id::makeFromBlankNodeIndex(
+          localVocab->getBlankNodeIndex(blankNodeManagerPtr));
+    }
+    tc = it->second;
   } else {
     throw std::runtime_error(absl::StrCat("Type ", type,
                                           " is undefined. The binding is: '",

commit 111d185a394a8e2be00bb8ff2d1d90d37d54ce9d

Also merge the `LocalBlankNodeManager`s when a `LocalVocab` is merged. (#1617)
This resolves a bug that was introduced in #1504, where the `LocalBlankNodeManager` of a `LocalVocab` was not accounted for in the merge functions of the `LocalVocab`.

src/engine/LocalVocab.cpp
@@ -82,7 +82,9 @@ BlankNodeIndex LocalVocab::getBlankNodeIndex(
   AD_CONTRACT_CHECK(blankNodeManager);
   // Initialize the `localBlankNodeManager_` if it doesn't exist yet.
   if (!localBlankNodeManager_) [[unlikely]] {
-    localBlankNodeManager_.emplace(blankNodeManager);
+    localBlankNodeManager_ =
+        std::make_shared<ad_utility::BlankNodeManager::LocalBlankNodeManager>(
+            blankNodeManager);
   }
   return BlankNodeIndex::make(localBlankNodeManager_->getId());
 }

src/engine/LocalVocab.h
@@ -5,8 +5,10 @@
 
 #pragma once
 
+#include <algorithm>
 #include <cstdlib>
 #include <memory>
+#include <ranges>
 #include <span>
 #include <string>
 #include <vector>
@@ -48,7 +50,7 @@ class LocalVocab {
 
   // Each `LocalVocab` has its own `LocalBlankNodeManager` to generate blank
   // nodes when needed (e.g., when parsing the result of a SERVICE query).
-  std::optional<ad_utility::BlankNodeManager::LocalBlankNodeManager>
+  std::shared_ptr<ad_utility::BlankNodeManager::LocalBlankNodeManager>
       localBlankNodeManager_;
 
  public:
@@ -116,6 +118,26 @@ class LocalVocab {
       *inserter = vocab.primaryWordSet_;
       size_ += vocab.size_;
     }
+
+    // Also merge the `vocabs` `LocalBlankNodeManager`s, if they exist.
+    using LocalBlankNodeManager =
+        ad_utility::BlankNodeManager::LocalBlankNodeManager;
+    auto localManagersView =
+        vocabs |
+        std::views::transform([](const LocalVocab& vocab) -> const auto& {
+          return vocab.localBlankNodeManager_;
+        });
+
+    auto it = std::ranges::find_if(localManagersView,
+                                   [](const auto& l) { return l != nullptr; });
+    if (it == localManagersView.end()) {
+      return;
+    }
+    if (!localBlankNodeManager_) {
+      localBlankNodeManager_ =
+          std::make_shared<LocalBlankNodeManager>((*it)->blankNodeManager());
+    }
+    localBlankNodeManager_->mergeWith(localManagersView);
   }
 
   // Create a new local vocab with empty set and other sets that are the union

src/util/BlankNodeManager.cpp
@@ -4,6 +4,8 @@
 
 #include "util/BlankNodeManager.h"
 
+#include "util/Exception.h"
+
 namespace ad_utility {
 
 // _____________________________________________________________________________
@@ -42,29 +44,27 @@ BlankNodeManager::LocalBlankNodeManager::LocalBlankNodeManager(
     : blankNodeManager_(blankNodeManager) {}
 
 // _____________________________________________________________________________
-BlankNodeManager::LocalBlankNodeManager::~LocalBlankNodeManager() {
-  auto ptr = blankNodeManager_->usedBlocksSet_.wlock();
-  for (const auto& block : blocks_) {
-    AD_CONTRACT_CHECK(ptr->contains(block.blockIdx_));
-    ptr->erase(block.blockIdx_);
-  }
-}
-
-// _____________________________________________________________________________
 uint64_t BlankNodeManager::LocalBlankNodeManager::getId() {
-  if (blocks_.empty() || blocks_.back().nextIdx_ == idxAfterCurrentBlock_) {
-    blocks_.emplace_back(blankNodeManager_->allocateBlock());
-    idxAfterCurrentBlock_ = blocks_.back().nextIdx_ + blockSize_;
+  if (blocks_->empty() || blocks_->back().nextIdx_ == idxAfterCurrentBlock_) {
+    blocks_->emplace_back(blankNodeManager_->allocateBlock());
+    idxAfterCurrentBlock_ = blocks_->back().nextIdx_ + blockSize_;
   }
-  return blocks_.back().nextIdx_++;
+  return blocks_->back().nextIdx_++;
 }
 
 // _____________________________________________________________________________
 bool BlankNodeManager::LocalBlankNodeManager::containsBlankNodeIndex(
     uint64_t index) const {
-  return std::ranges::any_of(blocks_, [index](const Block& block) {
+  auto containsIndex = [index](const Block& block) {
     return index >= block.startIdx_ && index < block.nextIdx_;
-  });
+  };
+
+  return std::ranges::any_of(*blocks_, containsIndex) ||
+         std::ranges::any_of(
+             otherBlocks_,
+             [&](const std::shared_ptr<const std::vector<Block>>& blocks) {
+               return std::ranges::any_of(*blocks, containsIndex);
+             });
 }
 
 }  // namespace ad_utility

src/util/BlankNodeManager.h
@@ -58,6 +58,7 @@ class BlankNodeManager {
 
    public:
     ~Block() = default;
+
     // The index of this block.
     const uint64_t blockIdx_;
 
@@ -71,7 +72,7 @@ class BlankNodeManager {
   class LocalBlankNodeManager {
    public:
     explicit LocalBlankNodeManager(BlankNodeManager* blankNodeManager);
-    ~LocalBlankNodeManager();
+    ~LocalBlankNodeManager() = default;
 
     // No copy, as the managed blocks should not be duplicated.
     LocalBlankNodeManager(const LocalBlankNodeManager&) = delete;
@@ -87,15 +88,45 @@ class BlankNodeManager {
     // Return true iff the `index` was returned by a previous call to `getId()`.
     bool containsBlankNodeIndex(uint64_t index) const;
 
-   private:
-    // Reserved blocks.
-    std::vector<BlankNodeManager::Block> blocks_;
+    // Merge passed `LocalBlankNodeManager`s to keep alive their reserved
+    // BlankNodeIndex blocks.
+    template <std::ranges::range R>
+    void mergeWith(const R& localBlankNodeManagers) {
+      auto inserter = std::back_inserter(otherBlocks_);
+      for (const auto& l : localBlankNodeManagers) {
+        if (l == nullptr) {
+          continue;
+        }
+        std::ranges::copy(l->otherBlocks_, inserter);
+        *inserter = l->blocks_;
+      }
+    }
 
+    // Getter for the `blankNodeManager_` pointer required in
+    // `LocalVocab::mergeWith`.
+    BlankNodeManager* blankNodeManager() const { return blankNodeManager_; }
+
+   private:
     // Reference to the BlankNodeManager, used to free the reserved blocks.
     BlankNodeManager* blankNodeManager_;
 
+    // Reserved blocks.
+    using Blocks = std::vector<BlankNodeManager::Block>;
+    std::shared_ptr<Blocks> blocks_{
+        new Blocks(), [blankNodeManager = blankNodeManager()](auto blocksPtr) {
+          auto ptr = blankNodeManager->usedBlocksSet_.wlock();
+          for (const auto& block : *blocksPtr) {
+            AD_CONTRACT_CHECK(ptr->contains(block.blockIdx_));
+            ptr->erase(block.blockIdx_);
+          }
+          delete blocksPtr;
+        }};
+
     // The first index after the current Block.
     uint64_t idxAfterCurrentBlock_{0};
+
+    // Blocks merged from other `LocalBlankNodeManager`s.
+    std::vector<std::shared_ptr<const Blocks>> otherBlocks_;
 
     FRIEND_TEST(BlankNodeManager, LocalBlankNodeManagerGetID);
   };

test/BlankNodeManagerTest.cpp
@@ -37,24 +37,32 @@ TEST(BlankNodeManager, blockAllocationAndFree) {
 // _____________________________________________________________________________
 TEST(BlankNodeManager, LocalBlankNodeManagerGetID) {
   BlankNodeManager bnm(0);
-  BlankNodeManager::LocalBlankNodeManager l(&bnm);
+  auto l = std::make_shared<BlankNodeManager::LocalBlankNodeManager>(&bnm);
 
   // initially the LocalBlankNodeManager doesn't have any blocks
-  EXPECT_EQ(l.blocks_.size(), 0);
+  EXPECT_EQ(l->blocks_->size(), 0);
 
   // A new Block is allocated, if
   // no blocks are allocated yet
-  uint64_t id = l.getId();
-  EXPECT_EQ(l.blocks_.size(), 1);
-  EXPECT_TRUE(l.containsBlankNodeIndex(id));
-  EXPECT_FALSE(l.containsBlankNodeIndex(id + 1));
-  EXPECT_FALSE(l.containsBlankNodeIndex(id - 1));
+  uint64_t id = l->getId();
+  EXPECT_EQ(l->blocks_->size(), 1);
+  EXPECT_TRUE(l->containsBlankNodeIndex(id));
+  EXPECT_FALSE(l->containsBlankNodeIndex(id + 1));
+  EXPECT_FALSE(l->containsBlankNodeIndex(id - 1));
 
   // or the ids of the last block are all used
-  l.blocks_.back().nextIdx_ = id + BlankNodeManager::blockSize_;
-  id = l.getId();
-  EXPECT_TRUE(l.containsBlankNodeIndex(id));
-  EXPECT_EQ(l.blocks_.size(), 2);
+  l->blocks_->back().nextIdx_ = id + BlankNodeManager::blockSize_;
+  id = l->getId();
+  EXPECT_TRUE(l->containsBlankNodeIndex(id));
+  EXPECT_EQ(l->blocks_->size(), 2);
+
+  // The `LocalBlankNodeManager` still works when recursively merged.
+  std::vector itSelf{l};
+  l->mergeWith(itSelf);
+
+  EXPECT_TRUE(l->containsBlankNodeIndex(id));
+  EXPECT_TRUE(l->containsBlankNodeIndex(l->getId()));
+  EXPECT_EQ(l->blocks_, l->otherBlocks_[0]);
 }
 
 // _____________________________________________________________________________

test/LocalVocabTest.cpp
@@ -134,6 +134,14 @@ TEST(LocalVocab, clone) {
   for (size_t i = 0; i < inputWords.size(); ++i) {
     EXPECT_EQ(*indices[i], inputWords[i]);
   }
+
+  // Test that a BlankNodeIndex obtained by a `LocalVocab` is also contained
+  // in the clone.
+  ad_utility::BlankNodeManager bnm;
+  LocalVocab v;
+  auto id = v.getBlankNodeIndex(&bnm);
+  LocalVocab vClone = v.clone();
+  EXPECT_TRUE(vClone.isBlankNodeIndexContained(id));
 }
 // _____________________________________________________________________________
 TEST(LocalVocab, merge) {
@@ -162,6 +170,30 @@ TEST(LocalVocab, merge) {
   EXPECT_EQ(*indices[1], lit("twoA"));
   EXPECT_EQ(*indices[2], lit("oneB"));
   EXPECT_EQ(*indices[3], lit("twoB"));
+
+  // Test that the `LocalBlankNodeManager` of vocabs is merged correctly.
+  ad_utility::BlankNodeManager bnm;
+  LocalVocab localVocabMerged2;
+  BlankNodeIndex id;
+  {
+    LocalVocab vocC, vocD;
+    id = vocC.getBlankNodeIndex(&bnm);
+    auto vocabs2 = std::vector{&std::as_const(vocC), &std::as_const(vocD)};
+    localVocabMerged2 = LocalVocab::merge(vocabs2);
+  }
+  EXPECT_TRUE(localVocabMerged2.isBlankNodeIndexContained(id));
+
+  LocalVocab vocE, vocF;
+  auto id2 = vocE.getBlankNodeIndex(&bnm);
+  auto vocabs3 =
+      std::vector{&std::as_const(localVocabMerged2), &std::as_const(vocF)};
+  vocE.mergeWith(vocabs3 | std::views::transform(
+                               [](const LocalVocab* l) -> const LocalVocab& {
+                                 return *l;
+                               }));
+  EXPECT_TRUE(vocE.isBlankNodeIndexContained(id));
+  EXPECT_TRUE(localVocabMerged2.isBlankNodeIndexContained(id));
+  EXPECT_TRUE(vocE.isBlankNodeIndexContained(id2));
 }
 
 // _____________________________________________________________________________