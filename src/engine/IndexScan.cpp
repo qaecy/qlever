qaecy/qlever

commit c354075b69bbd6bd7785215e41123ef74c21135b

Implement block prefiltering for `Join`s of a lazy result + `IndexScan` (#1647)
With this PR, a join between a lazy intermediate result and an `IndexScan` prefilters the blocks of the `IndexScan` before reading them. Before this PR, this prefilter was only applied to joins between two `IndexScan`s and between an `IndexScan` and a fully materialized intermediate result.

src/engine/IndexScan.cpp
@@ -4,6 +4,7 @@
 
 #include "engine/IndexScan.h"
 
+#include <absl/container/inlined_vector.h>
 #include <absl/strings/str_join.h>
 
 #include <boost/optional.hpp>
@@ -14,6 +15,7 @@
 #include "parser/ParsedQuery.h"
 
 using std::string;
+using LazyScanMetadata = CompressedRelationReader::LazyScanMetadata;
 
 // _____________________________________________________________________________
 // Return the number of `Variables` given the `TripleComponent` values for
@@ -442,15 +444,211 @@ Permutation::IdTableGenerator IndexScan::lazyScanForJoinOfColumnWithScan(
   AD_CORRECTNESS_CHECK(numVariables_ <= 3 && numVariables_ > 0);
   AD_CONTRACT_CHECK(joinColumn.empty() || !joinColumn[0].isUndefined());
 
-  auto metaBlocks1 = getMetadataForScan();
+  auto metaBlocks = getMetadataForScan();
 
-  if (!metaBlocks1.has_value()) {
+  if (!metaBlocks.has_value()) {
     return {};
   }
   auto blocks = CompressedRelationReader::getBlocksForJoin(joinColumn,
-                                                           metaBlocks1.value());
+                                                           metaBlocks.value());
 
   auto result = getLazyScan(blocks);
-  result.details().numBlocksAll_ = metaBlocks1.value().blockMetadata_.size();
+  result.details().numBlocksAll_ = metaBlocks.value().blockMetadata_.size();
   return result;
+}
+
+// _____________________________________________________________________________
+void IndexScan::updateRuntimeInfoForLazyScan(const LazyScanMetadata& metadata) {
+  updateRuntimeInformationWhenOptimizedOut(
+      RuntimeInformation::Status::lazilyMaterialized);
+  auto& rti = runtimeInfo();
+  rti.numRows_ = metadata.numElementsYielded_;
+  rti.totalTime_ = metadata.blockingTime_;
+  rti.addDetail("num-blocks-read", metadata.numBlocksRead_);
+  rti.addDetail("num-blocks-all", metadata.numBlocksAll_);
+  rti.addDetail("num-elements-read", metadata.numElementsRead_);
+
+  // Add more details, but only if the respective value is non-zero.
+  auto updateIfPositive = [&rti](const auto& value, const std::string& key) {
+    if (value > 0) {
+      rti.addDetail(key, value);
+    }
+  };
+  updateIfPositive(metadata.numBlocksSkippedBecauseOfGraph_,
+                   "num-blocks-skipped-graph");
+  updateIfPositive(metadata.numBlocksPostprocessed_,
+                   "num-blocks-postprocessed");
+  updateIfPositive(metadata.numBlocksWithUpdate_, "num-blocks-with-update");
+}
+
+// Store a Generator and its corresponding iterator as well as unconsumed values
+// resulting from the generator.
+struct IndexScan::SharedGeneratorState {
+  // The generator that yields the tables to be joined with the index scan.
+  Result::Generator generator_;
+  // The column index of the join column in the tables yielded by the generator.
+  ColumnIndex joinColumn_;
+  // Metadata and blocks of this index scan.
+  Permutation::MetadataAndBlocks metaBlocks_;
+  // The iterator of the generator that is currently being consumed.
+  std::optional<Result::Generator::iterator> iterator_ = std::nullopt;
+  // Values returned by the generator that have not been re-yielded yet.
+  // Typically we expect only 3 or less values to be prefetched (this is an
+  // implementation detail of `BlockZipperJoinImpl`).
+  using PrefetchStorage = absl::InlinedVector<Result::IdTableVocabPair, 3>;
+  PrefetchStorage prefetchedValues_{};
+  // Metadata of blocks that still need to be read.
+  std::vector<CompressedBlockMetadata> pendingBlocks_{};
+  // The index of the last matching block that was found using the join column.
+  std::optional<size_t> lastBlockIndex_ = std::nullopt;
+  // Indicates if the generator has yielded any undefined values.
+  bool hasUndef_ = false;
+  // Indicates if the generator has been fully consumed.
+  bool doneFetching_ = false;
+
+  // Advance the `iterator` to the next non-empty table. Set `hasUndef_` to true
+  // if the first table is undefined. Also set `doneFetching_` if the generator
+  // has been fully consumed.
+  void advanceInputToNextNonEmptyTable() {
+    bool firstStep = !iterator_.has_value();
+    if (iterator_.has_value()) {
+      ++iterator_.value();
+    } else {
+      iterator_ = generator_.begin();
+    }
+    auto& iterator = iterator_.value();
+    while (iterator != generator_.end()) {
+      if (!iterator->idTable_.empty()) {
+        break;
+      }
+      ++iterator;
+    }
+    doneFetching_ = iterator_ == generator_.end();
+    // Set the undef flag if the first table is undefined.
+    if (firstStep) {
+      hasUndef_ =
+          !doneFetching_ && iterator->idTable_.at(0, joinColumn_).isUndefined();
+    }
+  }
+
+  // Consume the next non-empty table from the generator and calculate the next
+  // matching blocks from the index scan.
+  void fetch() {
+    advanceInputToNextNonEmptyTable();
+    if (doneFetching_) {
+      return;
+    }
+    auto& idTable = iterator_.value()->idTable_;
+    auto joinColumn = idTable.getColumn(joinColumn_);
+    AD_EXPENSIVE_CHECK(std::ranges::is_sorted(joinColumn));
+    AD_CORRECTNESS_CHECK(!joinColumn.empty());
+    // Skip processing for undef case, it will be handled differently
+    if (hasUndef_) {
+      return;
+    }
+    AD_CORRECTNESS_CHECK(!joinColumn[0].isUndefined());
+    auto newBlocks =
+        CompressedRelationReader::getBlocksForJoin(joinColumn, metaBlocks_);
+    if (newBlocks.empty()) {
+      // The current input table matches no blocks, so we don't have to yield
+      // it.
+      return;
+    }
+    prefetchedValues_.push_back(std::move(*iterator_.value()));
+    // Find first value that differs from the last one that was used to find
+    // matching blocks.
+    auto startIterator =
+        lastBlockIndex_.has_value()
+            ? std::ranges::upper_bound(newBlocks, lastBlockIndex_.value(), {},
+                                       &CompressedBlockMetadata::blockIndex_)
+            : newBlocks.begin();
+    lastBlockIndex_ = newBlocks.back().blockIndex_;
+    std::ranges::move(startIterator, newBlocks.end(),
+                      std::back_inserter(pendingBlocks_));
+  }
+
+  // Check if there are any undefined values yielded by the original generator.
+  // If the generator hasn't been started to get consumed, this will start it.
+  bool hasUndef() {
+    if (!iterator_.has_value()) {
+      fetch();
+    }
+    return hasUndef_;
+  }
+};
+
+// _____________________________________________________________________________
+Result::Generator IndexScan::createPrefilteredJoinSide(
+    std::shared_ptr<SharedGeneratorState> innerState) {
+  if (innerState->hasUndef()) {
+    AD_CORRECTNESS_CHECK(innerState->prefetchedValues_.empty());
+    for (auto& value : std::ranges::subrange{innerState->iterator_.value(),
+                                             innerState->generator_.end()}) {
+      co_yield value;
+    }
+    co_return;
+  }
+  auto& prefetchedValues = innerState->prefetchedValues_;
+  while (true) {
+    if (prefetchedValues.empty()) {
+      if (innerState->doneFetching_) {
+        co_return;
+      }
+      innerState->fetch();
+      AD_CORRECTNESS_CHECK(!prefetchedValues.empty() ||
+                           innerState->doneFetching_);
+    }
+    // Make a defensive copy of the values to avoid modification during
+    // iteration when yielding.
+    auto copy = std::move(prefetchedValues);
+    // Moving out does not necessarily clear the values, so we do it explicitly.
+    prefetchedValues.clear();
+    for (auto& value : copy) {
+      co_yield value;
+    }
+  }
+}
+
+// _____________________________________________________________________________
+Result::Generator IndexScan::createPrefilteredIndexScanSide(
+    std::shared_ptr<SharedGeneratorState> innerState) {
+  if (innerState->hasUndef()) {
+    for (auto& pair : chunkedIndexScan()) {
+      co_yield pair;
+    }
+    co_return;
+  }
+  LazyScanMetadata metadata;
+  auto& pendingBlocks = innerState->pendingBlocks_;
+  while (true) {
+    if (pendingBlocks.empty()) {
+      if (innerState->doneFetching_) {
+        metadata.numBlocksAll_ = innerState->metaBlocks_.blockMetadata_.size();
+        updateRuntimeInfoForLazyScan(metadata);
+        co_return;
+      }
+      innerState->fetch();
+    }
+    auto scan = getLazyScan(std::move(pendingBlocks));
+    AD_CORRECTNESS_CHECK(pendingBlocks.empty());
+    for (IdTable& idTable : scan) {
+      co_yield {std::move(idTable), LocalVocab{}};
+    }
+    metadata.aggregate(scan.details());
+  }
+}
+
+// _____________________________________________________________________________
+std::pair<Result::Generator, Result::Generator> IndexScan::prefilterTables(
+    Result::Generator input, ColumnIndex joinColumn) {
+  AD_CORRECTNESS_CHECK(numVariables_ <= 3 && numVariables_ > 0);
+  auto metaBlocks = getMetadataForScan();
+
+  if (!metaBlocks.has_value()) {
+    return {Result::Generator{}, Result::Generator{}};
+  }
+  auto state = std::make_shared<SharedGeneratorState>(
+      std::move(input), joinColumn, std::move(metaBlocks.value()));
+  return {createPrefilteredJoinSide(state),
+          createPrefilteredIndexScanSide(state)};
 }

test/engine/IndexScanTest.cpp
@@ -759,3 +759,322 @@ TEST(IndexScan, checkEvaluationWithPrefiltering) {
       pr(andExpr(gt(IntId(10)), lt(IntId(194))), Variable{"?price"}),
       {I(10), I(12), I(18), I(22), I(25), I(147), I(189), I(194)});
 }
+
+class IndexScanWithLazyJoin : public ::testing::TestWithParam<bool> {
+ protected:
+  QueryExecutionContext* qec_ = nullptr;
+
+  void SetUp() override {
+    std::string kg =
+        "<a> <p> <A> . <a> <p> <A2> . "
+        "<b> <p> <B> . <b> <p> <B2> . "
+        "<c> <p> <C> . <c> <p> <C2> . "
+        "<b> <q> <xb> . <b> <q> <xb2> .";
+    qec_ = getQec(std::move(kg));
+  }
+
+  // Convert a TripleComponent to a ValueId.
+  Id toValueId(const TripleComponent& tc) const {
+    return tc.toValueId(qec_->getIndex().getVocab()).value();
+  }
+
+  // Create an id table with a single column from a vector of
+  // `TripleComponent`s.
+  IdTable makeIdTable(std::vector<TripleComponent> entries) const {
+    IdTable result{1, makeAllocator()};
+    result.reserve(entries.size());
+    for (const TripleComponent& entry : entries) {
+      result.emplace_back();
+      result.back()[0] = toValueId(entry);
+    }
+    return result;
+  }
+
+  // Convert a vector of a tuple of triples to an id table.
+  IdTable tableFromTriples(
+      std::vector<std::array<TripleComponent, 2>> triples) const {
+    IdTable result{2, makeAllocator()};
+    result.reserve(triples.size());
+    for (const auto& triple : triples) {
+      result.emplace_back();
+      result.back()[0] = toValueId(triple.at(0));
+      result.back()[1] = toValueId(triple.at(1));
+    }
+    return result;
+  }
+
+  // Create a common `IndexScan` instance.
+  IndexScan makeScan() const {
+    SparqlTriple xpy{Tc{Var{"?x"}}, "<p>", Tc{Var{"?y"}}};
+    // We need to scan all the blocks that contain the `<p>` predicate.
+    return IndexScan{qec_, Permutation::PSO, xpy};
+  }
+
+  // Consume generator `first` first and store it in a vector, then do the same
+  // with `second`.
+  static std::pair<std::vector<Result::IdTableVocabPair>,
+                   std::vector<Result::IdTableVocabPair>>
+  consumeSequentially(Result::Generator first, Result::Generator second) {
+    std::vector<Result::IdTableVocabPair> firstResult;
+    std::vector<Result::IdTableVocabPair> secondResult;
+
+    for (Result::IdTableVocabPair& element : first) {
+      firstResult.push_back(std::move(element));
+    }
+    for (Result::IdTableVocabPair& element : second) {
+      secondResult.push_back(std::move(element));
+    }
+    return {std::move(firstResult), std::move(secondResult)};
+  }
+
+  // Consume the generators and store the results in vectors using the
+  // parameterized strategy.
+  static std::pair<std::vector<Result::IdTableVocabPair>,
+                   std::vector<Result::IdTableVocabPair>>
+  consumeGenerators(
+      std::pair<Result::Generator, Result::Generator> generatorPair) {
+    std::vector<Result::IdTableVocabPair> joinSideResults;
+    std::vector<Result::IdTableVocabPair> scanResults;
+
+    bool rightFirst = GetParam();
+    if (rightFirst) {
+      std::tie(scanResults, joinSideResults) = consumeSequentially(
+          std::move(generatorPair.second), std::move(generatorPair.first));
+    } else {
+      std::tie(joinSideResults, scanResults) = consumeSequentially(
+          std::move(generatorPair.first), std::move(generatorPair.second));
+    }
+    return {std::move(joinSideResults), std::move(scanResults)};
+  }
+};
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin, prefilterTablesDoesFilterCorrectly) {
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = [](auto* self) -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p1{self->makeIdTable({iri("<a>"), iri("<c>")}), LocalVocab{}};
+    co_yield p1;
+    P p2{self->makeIdTable({iri("<c>")}), LocalVocab{}};
+    co_yield p2;
+    LocalVocab vocab;
+    vocab.getIndexAndAddIfNotContained(LocalVocabEntry{
+        ad_utility::triple_component::Literal::literalWithoutQuotes("Test")});
+    P p3{self->makeIdTable({iri("<c>"), iri("<q>"), iri("<xb>")}),
+         std::move(vocab)};
+    co_yield p3;
+  };
+
+  auto [joinSideResults, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(this), 0));
+
+  ASSERT_EQ(scanResults.size(), 2);
+  ASSERT_EQ(joinSideResults.size(), 3);
+
+  EXPECT_TRUE(scanResults.at(0).localVocab_.empty());
+  EXPECT_TRUE(joinSideResults.at(0).localVocab_.empty());
+
+  EXPECT_TRUE(scanResults.at(1).localVocab_.empty());
+  EXPECT_TRUE(joinSideResults.at(1).localVocab_.empty());
+
+  EXPECT_FALSE(joinSideResults.at(2).localVocab_.empty());
+
+  EXPECT_EQ(
+      scanResults.at(0).idTable_,
+      tableFromTriples({{iri("<a>"), iri("<A>")}, {iri("<a>"), iri("<A2>")}}));
+  EXPECT_EQ(joinSideResults.at(0).idTable_,
+            makeIdTable({iri("<a>"), iri("<c>")}));
+
+  EXPECT_EQ(
+      scanResults.at(1).idTable_,
+      tableFromTriples({{iri("<c>"), iri("<C>")}, {iri("<c>"), iri("<C2>")}}));
+  EXPECT_EQ(joinSideResults.at(1).idTable_, makeIdTable({iri("<c>")}));
+
+  EXPECT_EQ(joinSideResults.at(2).idTable_,
+            makeIdTable({iri("<c>"), iri("<q>"), iri("<xb>")}));
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin,
+       prefilterTablesDoesFilterCorrectlyWithOverlappingValues) {
+  std::string kg = "<a> <p> <A> . <b> <p> <B>. ";
+  qec_ = getQec(std::move(kg));
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = [](auto* self) -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p1{self->makeIdTable({iri("<a>")}), LocalVocab{}};
+    co_yield p1;
+    P p2{self->makeIdTable({iri("<b>")}), LocalVocab{}};
+    co_yield p2;
+  };
+
+  auto [joinSideResults, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(this), 0));
+
+  ASSERT_EQ(scanResults.size(), 1);
+  ASSERT_EQ(joinSideResults.size(), 2);
+
+  EXPECT_TRUE(scanResults.at(0).localVocab_.empty());
+  EXPECT_TRUE(joinSideResults.at(0).localVocab_.empty());
+  EXPECT_TRUE(joinSideResults.at(1).localVocab_.empty());
+
+  EXPECT_EQ(
+      scanResults.at(0).idTable_,
+      tableFromTriples({{iri("<a>"), iri("<A>")}, {iri("<b>"), iri("<B>")}}));
+  EXPECT_EQ(joinSideResults.at(0).idTable_, makeIdTable({iri("<a>")}));
+
+  EXPECT_EQ(joinSideResults.at(1).idTable_, makeIdTable({iri("<b>")}));
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin,
+       prefilterTablesDoesFilterCorrectlyWithSkipTablesWithoutMatchingBlock) {
+  std::string kg = "<a> <p> <A> . <b> <p> <B>. ";
+  qec_ = getQec(std::move(kg));
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = []() -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p1{makeIdTableFromVector({{Id::makeFromBool(true)}}), LocalVocab{}};
+    co_yield p1;
+  };
+
+  auto [joinSideResults, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(), 0));
+
+  ASSERT_EQ(scanResults.size(), 0);
+  ASSERT_EQ(joinSideResults.size(), 0);
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin, prefilterTablesDoesNotFilterOnUndefined) {
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = [](auto* self) -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p1{IdTable{1, makeAllocator()}, LocalVocab{}};
+    co_yield p1;
+    P p2{makeIdTableFromVector({{Id::makeUndefined()}}), LocalVocab{}};
+    co_yield p2;
+    P p3{makeIdTableFromVector({{Id::makeUndefined()}}), LocalVocab{}};
+    co_yield p3;
+    P p4{IdTable{1, makeAllocator()}, LocalVocab{}};
+    co_yield p4;
+    P p5{self->makeIdTable({iri("<a>"), iri("<c>")}), LocalVocab{}};
+    co_yield p5;
+    P p6{self->makeIdTable({iri("<c>"), iri("<q>"), iri("<xb>")}),
+         LocalVocab{}};
+    co_yield p6;
+    P p7{IdTable{1, makeAllocator()}, LocalVocab{}};
+    co_yield p7;
+  };
+
+  auto [_, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(this), 0));
+
+  ASSERT_EQ(scanResults.size(), 3);
+  EXPECT_TRUE(scanResults.at(0).localVocab_.empty());
+  EXPECT_TRUE(scanResults.at(1).localVocab_.empty());
+  EXPECT_TRUE(scanResults.at(2).localVocab_.empty());
+
+  EXPECT_EQ(
+      scanResults.at(0).idTable_,
+      tableFromTriples({{iri("<a>"), iri("<A>")}, {iri("<a>"), iri("<A2>")}}));
+
+  EXPECT_EQ(
+      scanResults.at(1).idTable_,
+      tableFromTriples({{iri("<b>"), iri("<B>")}, {iri("<b>"), iri("<B2>")}}));
+
+  EXPECT_EQ(
+      scanResults.at(2).idTable_,
+      tableFromTriples({{iri("<c>"), iri("<C>")}, {iri("<c>"), iri("<C2>")}}));
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin, prefilterTablesDoesNotFilterWithSingleUndefined) {
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = []() -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p1{makeIdTableFromVector({{Id::makeUndefined()}}), LocalVocab{}};
+    co_yield p1;
+  };
+
+  auto [_, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(), 0));
+
+  ASSERT_EQ(scanResults.size(), 3);
+  EXPECT_TRUE(scanResults.at(0).localVocab_.empty());
+  EXPECT_TRUE(scanResults.at(1).localVocab_.empty());
+  EXPECT_TRUE(scanResults.at(2).localVocab_.empty());
+
+  EXPECT_EQ(
+      scanResults.at(0).idTable_,
+      tableFromTriples({{iri("<a>"), iri("<A>")}, {iri("<a>"), iri("<A2>")}}));
+
+  EXPECT_EQ(
+      scanResults.at(1).idTable_,
+      tableFromTriples({{iri("<b>"), iri("<B>")}, {iri("<b>"), iri("<B2>")}}));
+
+  EXPECT_EQ(
+      scanResults.at(2).idTable_,
+      tableFromTriples({{iri("<c>"), iri("<C>")}, {iri("<c>"), iri("<C2>")}}));
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin, prefilterTablesWorksWithSingleEmptyTable) {
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = []() -> Result::Generator {
+    using P = Result::IdTableVocabPair;
+    P p{IdTable{1, makeAllocator()}, LocalVocab{}};
+    co_yield p;
+  };
+
+  auto [_, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(), 0));
+
+  ASSERT_EQ(scanResults.size(), 0);
+}
+
+// _____________________________________________________________________________
+TEST_P(IndexScanWithLazyJoin, prefilterTablesWorksWithEmptyGenerator) {
+  IndexScan scan = makeScan();
+
+  auto makeJoinSide = []() -> Result::Generator { co_return; };
+
+  auto [_, scanResults] =
+      consumeGenerators(scan.prefilterTables(makeJoinSide(), 0));
+
+  ASSERT_EQ(scanResults.size(), 0);
+}
+
+INSTANTIATE_TEST_SUITE_P(IndexScanWithLazyJoinSuite, IndexScanWithLazyJoin,
+                         ::testing::Bool(),
+                         [](const testing::TestParamInfo<bool>& info) {
+                           return info.param ? "RightSideFirst"
+                                             : "LeftSideFirst";
+                         });
+
+// _____________________________________________________________________________
+TEST(IndexScan, prefilterTablesWithEmptyIndexScanReturnsEmptyGenerators) {
+  auto* qec = getQec("<a> <p> <A>");
+  // Match with something that does not match.
+  SparqlTriple xpy{Tc{Var{"?x"}}, "<not_p>", Tc{Var{"?y"}}};
+  IndexScan scan{qec, Permutation::PSO, xpy};
+
+  auto makeJoinSide = []() -> Result::Generator {
+    ADD_FAILURE()
+        << "The generator should not be consumed when the IndexScan is empty."
+        << std::endl;
+    co_return;
+  };
+
+  auto [leftGenerator, rightGenerator] =
+      scan.prefilterTables(makeJoinSide(), 0);
+
+  EXPECT_EQ(leftGenerator.begin(), leftGenerator.end());
+  EXPECT_EQ(rightGenerator.begin(), rightGenerator.end());
+}

src/engine/IndexScan.h
@@ -99,7 +99,32 @@ class IndexScan final : public Operation {
   Permutation::IdTableGenerator lazyScanForJoinOfColumnWithScan(
       std::span<const Id> joinColumn) const;
 
+  // Return two generators, the first of which yields exactly the elements of
+  // `input` and the second of which yields the matching blocks, skipping the
+  // blocks consisting only of rows that don't match the tables yielded by
+  // `input` to speed up join algorithms when no undef values are presend. When
+  // there are undef values, the second generator represents the full index
+  // scan.
+  std::pair<Result::Generator, Result::Generator> prefilterTables(
+      Result::Generator input, ColumnIndex joinColumn);
+
  private:
+  // Implementation detail that allows to consume a generator from two other
+  // cooperating generators. Needs to be forward declared as it is used by
+  // several member functions below.
+  struct SharedGeneratorState;
+
+  // Helper function that creates a generator that re-yields the generator
+  // wrapped by `innerState`.
+  static Result::Generator createPrefilteredJoinSide(
+      std::shared_ptr<SharedGeneratorState> innerState);
+
+  // Helper function that creates a generator yielding prefiltered rows of this
+  // index scan according to the block metadata, that match the tables yielded
+  // by the generator wrapped by `innerState`.
+  Result::Generator createPrefilteredIndexScanSide(
+      std::shared_ptr<SharedGeneratorState> innerState);
+
   // TODO<joka921> Make the `getSizeEstimateBeforeLimit()` function `const` for
   // ALL the `Operations`.
   uint64_t getSizeEstimateBeforeLimit() override { return sizeEstimate_; }
@@ -144,6 +169,11 @@ class IndexScan final : public Operation {
   std::array<const TripleComponent* const, 3> getPermutedTriple() const;
   ScanSpecification getScanSpecification() const;
   ScanSpecificationAsTripleComponent getScanSpecificationTc() const;
+
+  // Set the runtime info of the `scanTree` when it was lazily executed during a
+  // join.
+  void updateRuntimeInfoForLazyScan(
+      const CompressedRelationReader::LazyScanMetadata& metadata);
 
  private:
   ProtoResult computeResult(bool requestLaziness) override;

commit d43989b2560985ddff30fea1d445c4e05e2ba58e

Fix the lazy block prefilter (#1657)
This fixes a bug introduced in #1647, which led to a failure of ` AD_CORRECTNESS_CHECK(!prefetchedValues.empty() || innerState->doneFetching_);` in `IndexScan::createPrefilteredJoinSide`.

src/engine/IndexScan.cpp
@@ -532,39 +532,43 @@ struct IndexScan::SharedGeneratorState {
   }
 
   // Consume the next non-empty table from the generator and calculate the next
-  // matching blocks from the index scan.
+  // matching blocks from the index scan. This function guarantees that after
+  // it returns, both `prefetchedValues` and `pendingBlocks` contain at least
+  // one element.
   void fetch() {
-    advanceInputToNextNonEmptyTable();
-    if (doneFetching_) {
-      return;
+    while (prefetchedValues_.empty() || pendingBlocks_.empty()) {
+      advanceInputToNextNonEmptyTable();
+      if (doneFetching_) {
+        return;
+      }
+      auto& idTable = iterator_.value()->idTable_;
+      auto joinColumn = idTable.getColumn(joinColumn_);
+      AD_EXPENSIVE_CHECK(std::ranges::is_sorted(joinColumn));
+      AD_CORRECTNESS_CHECK(!joinColumn.empty());
+      // Skip processing for undef case, it will be handled differently
+      if (hasUndef_) {
+        return;
+      }
+      AD_CORRECTNESS_CHECK(!joinColumn[0].isUndefined());
+      auto newBlocks =
+          CompressedRelationReader::getBlocksForJoin(joinColumn, metaBlocks_);
+      if (newBlocks.empty()) {
+        // The current input table matches no blocks, so we don't have to yield
+        // it.
+        continue;
+      }
+      prefetchedValues_.push_back(std::move(*iterator_.value()));
+      // Find first value that differs from the last one that was used to find
+      // matching blocks.
+      auto startIterator =
+          lastBlockIndex_.has_value()
+              ? std::ranges::upper_bound(newBlocks, lastBlockIndex_.value(), {},
+                                         &CompressedBlockMetadata::blockIndex_)
+              : newBlocks.begin();
+      lastBlockIndex_ = newBlocks.back().blockIndex_;
+      std::ranges::move(startIterator, newBlocks.end(),
+                        std::back_inserter(pendingBlocks_));
     }
-    auto& idTable = iterator_.value()->idTable_;
-    auto joinColumn = idTable.getColumn(joinColumn_);
-    AD_EXPENSIVE_CHECK(std::ranges::is_sorted(joinColumn));
-    AD_CORRECTNESS_CHECK(!joinColumn.empty());
-    // Skip processing for undef case, it will be handled differently
-    if (hasUndef_) {
-      return;
-    }
-    AD_CORRECTNESS_CHECK(!joinColumn[0].isUndefined());
-    auto newBlocks =
-        CompressedRelationReader::getBlocksForJoin(joinColumn, metaBlocks_);
-    if (newBlocks.empty()) {
-      // The current input table matches no blocks, so we don't have to yield
-      // it.
-      return;
-    }
-    prefetchedValues_.push_back(std::move(*iterator_.value()));
-    // Find first value that differs from the last one that was used to find
-    // matching blocks.
-    auto startIterator =
-        lastBlockIndex_.has_value()
-            ? std::ranges::upper_bound(newBlocks, lastBlockIndex_.value(), {},
-                                       &CompressedBlockMetadata::blockIndex_)
-            : newBlocks.begin();
-    lastBlockIndex_ = newBlocks.back().blockIndex_;
-    std::ranges::move(startIterator, newBlocks.end(),
-                      std::back_inserter(pendingBlocks_));
   }
 
   // Check if there are any undefined values yielded by the original generator.

test/engine/IndexScanTest.cpp
@@ -939,6 +939,8 @@ TEST_P(IndexScanWithLazyJoin,
     using P = Result::IdTableVocabPair;
     P p1{makeIdTableFromVector({{Id::makeFromBool(true)}}), LocalVocab{}};
     co_yield p1;
+    P p2{makeIdTableFromVector({{Id::makeFromBool(true)}}), LocalVocab{}};
+    co_yield p2;
   };
 
   auto [joinSideResults, scanResults] =