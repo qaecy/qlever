qaecy/qlever

commit a1bb9d0acbe919de683e7be06407f15891661c2a

Only compute the sibling of a `SERVICE` once (#1556)
Previously, the sibling of a `SERVICE` (see #1341 for details) was computed twice. This was not a problem for small siblings result, because they were cached, making the second computation actually a cheap cache read. It was however a problem if the result was too large for the cache. It then was computed twice, although only one of the computations was actually used, because the SERVICE discarded the result because it was too large.
This commit fixes this behavior. The sibling is now only computed once, and only gets explicitly shared with the `SERVICE` if it is sufficiently small.

src/engine/Join.cpp
@@ -14,6 +14,7 @@
 #include "engine/AddCombinedRowToTable.h"
 #include "engine/CallFixedSize.h"
 #include "engine/IndexScan.h"
+#include "engine/Service.h"
 #include "global/Constants.h"
 #include "global/Id.h"
 #include "global/RuntimeParameters.h"
@@ -150,6 +151,12 @@ ProtoResult Join::computeResult([[maybe_unused]] bool requestLaziness) {
       return {std::move(idTable), resultSortedOn(), LocalVocab{}};
     }
   }
+
+  // If one of the RootOperations is a Service, precompute the result of its
+  // sibling.
+  Service::precomputeSiblingResult(_left->getRootOperation(),
+                                   _right->getRootOperation(), false,
+                                   requestLaziness);
 
   std::shared_ptr<const Result> leftRes =
       leftResIfCached ? leftResIfCached : _left->getResult();

src/engine/Minus.cpp
@@ -5,6 +5,7 @@
 #include "Minus.h"
 
 #include "engine/CallFixedSize.h"
+#include "engine/Service.h"
 #include "util/Exception.h"
 
 using std::endl;
@@ -34,6 +35,12 @@ string Minus::getDescriptor() const { return "Minus"; }
 // _____________________________________________________________________________
 ProtoResult Minus::computeResult([[maybe_unused]] bool requestLaziness) {
   LOG(DEBUG) << "Minus result computation..." << endl;
+
+  // If the right of the RootOperations is a Service, precompute the result of
+  // its sibling.
+  Service::precomputeSiblingResult(_left->getRootOperation(),
+                                   _right->getRootOperation(), true,
+                                   requestLaziness);
 
   IdTable idTable{getExecutionContext()->getAllocator()};
   idTable.setNumColumns(getResultWidth());

src/engine/Operation.cpp
@@ -212,6 +212,13 @@ CacheValue Operation::runComputationAndPrepareForCache(
 // ________________________________________________________________________
 std::shared_ptr<const Result> Operation::getResult(
     bool isRoot, ComputationMode computationMode) {
+  // Use the precomputed Result if it exists.
+  if (precomputedResultBecauseSiblingOfService_.has_value()) {
+    auto result = std::move(precomputedResultBecauseSiblingOfService_).value();
+    precomputedResultBecauseSiblingOfService_.reset();
+    return result;
+  }
+
   ad_utility::Timer timer{ad_utility::Timer::Started};
 
   if (isRoot) {

src/engine/Operation.h
@@ -131,6 +131,12 @@ class Operation {
     return false;
   }
 
+  // See the member variable with the same name below for documentation.
+  std::optional<std::shared_ptr<const Result>>&
+  precomputedResultBecauseSiblingOfService() {
+    return precomputedResultBecauseSiblingOfService_;
+  }
+
   RuntimeInformation& runtimeInfo() const { return *_runtimeInfo; }
 
   std::shared_ptr<RuntimeInformation> getRuntimeInfoPointer() {
@@ -353,6 +359,11 @@ class Operation {
   // Recursively call a function on all children.
   template <typename F>
   void forAllDescendants(F f) const;
+
+  // Holds a precomputed Result of this operation if it is the sibling of a
+  // Service operation.
+  std::optional<std::shared_ptr<const Result>>
+      precomputedResultBecauseSiblingOfService_;
 
   std::shared_ptr<RuntimeInformation> _runtimeInfo =
       std::make_shared<RuntimeInformation>();

src/engine/OptionalJoin.cpp
@@ -8,6 +8,7 @@
 #include "engine/AddCombinedRowToTable.h"
 #include "engine/CallFixedSize.h"
 #include "engine/Engine.h"
+#include "engine/Service.h"
 #include "util/JoinAlgorithms/JoinAlgorithms.h"
 
 using std::endl;
@@ -91,6 +92,12 @@ string OptionalJoin::getDescriptor() const {
 // _____________________________________________________________________________
 ProtoResult OptionalJoin::computeResult([[maybe_unused]] bool requestLaziness) {
   LOG(DEBUG) << "OptionalJoin result computation..." << endl;
+
+  // If the right of the RootOperations is a Service, precompute the result of
+  // its sibling.
+  Service::precomputeSiblingResult(_left->getRootOperation(),
+                                   _right->getRootOperation(), true,
+                                   requestLaziness);
 
   IdTable idTable{getExecutionContext()->getAllocator()};
   idTable.setNumColumns(getResultWidth());

src/engine/QueryPlanner.cpp
@@ -1806,32 +1806,16 @@ std::vector<QueryPlanner::SubtreePlan> QueryPlanner::createJoinCandidates(
   // further into the query that optional should be resolved by now.
   AD_CONTRACT_CHECK(a.type != SubtreePlan::OPTIONAL);
   if (b.type == SubtreePlan::MINUS) {
-    if (auto opt = createSubtreeWithService<Minus>(a, b)) {
-      return {opt.value()};
-    }
     return {makeSubtreePlan<Minus>(_qec, a._qet, b._qet)};
   }
 
   // OPTIONAL JOINS are not symmetric!
   if (b.type == SubtreePlan::OPTIONAL) {
-    // If the OPTIONAL subtree's rootOperation is a SERVICE, try to simplify it
-    // using the result of the first subtree.
-    if (auto opt = createSubtreeWithService<OptionalJoin>(a, b)) {
-      return {opt.value()};
-    }
-
     // Join the two optional columns using an optional join
     return {makeSubtreePlan<OptionalJoin>(_qec, a._qet, b._qet)};
   }
 
   if (auto opt = createJoinWithPathSearch(a, b, jcs)) {
-    candidates.push_back(std::move(opt.value()));
-    return candidates;
-  }
-
-  // Check if one of the two Operations is a SERVICE. If so, we can try
-  // to simplify the Service Query using the result of the other operation.
-  if (auto opt = createJoinWithService(a, b, jcs)) {
     candidates.push_back(std::move(opt.value()));
     return candidates;
   }
@@ -2005,71 +1989,6 @@ auto QueryPlanner::createJoinWithHasPredicateScan(
   auto plan = makeSubtreePlan<HasPredicateScan>(
       qec, std::move(otherTree), otherTreeJoinColumn, std::move(object));
   mergeSubtreePlanIds(plan, a, b);
-  return plan;
-}
-
-// _____________________________________________________________________
-auto QueryPlanner::createJoinWithService(
-    const SubtreePlan& a, const SubtreePlan& b,
-    const std::vector<std::array<ColumnIndex, 2>>& jcs)
-    -> std::optional<SubtreePlan> {
-  // We can only proceed if exactly one of the inputs is a `SERVICE`.
-  auto aRootOp = std::dynamic_pointer_cast<Service>(a._qet->getRootOperation());
-  auto bRootOp = std::dynamic_pointer_cast<Service>(b._qet->getRootOperation());
-  if (static_cast<bool>(aRootOp) == static_cast<bool>(bRootOp)) {
-    return std::nullopt;
-  }
-
-  // Setup some variables that are agnostic of which of the two inputs is the
-  // serviceWithSibling clause, as these cases are completely symmetric.
-  const auto& [serviceIn, sibling, serviceIdx, siblingIdx] = [&]() {
-    // `std::tie` requires lvalue-references, that's why we define explicit
-    // variables for `0` and `1`.
-    static constexpr size_t zero = 0;
-    static constexpr size_t one = 1;
-    if (aRootOp) {
-      return std::tie(aRootOp, b, zero, one);
-    } else {
-      AD_CORRECTNESS_CHECK(bRootOp);
-      return std::tie(bRootOp, a, one, zero);
-    }
-  }();
-
-  auto serviceWithSibling =
-      makeSubtreePlan(serviceIn->createCopyWithSiblingTree(sibling._qet));
-  auto qec = serviceIn->getExecutionContext();
-
-  SubtreePlan plan =
-      jcs.size() == 1
-          ? makeSubtreePlan<Join>(qec, std::move(serviceWithSibling._qet),
-                                  sibling._qet, jcs[0][serviceIdx],
-                                  jcs[0][siblingIdx])
-          : makeSubtreePlan<MultiColumnJoin>(
-                qec, std::move(serviceWithSibling._qet), sibling._qet);
-  mergeSubtreePlanIds(plan, a, b);
-
-  return plan;
-}
-
-// _____________________________________________________________________
-template <typename Operation>
-auto QueryPlanner::createSubtreeWithService(const SubtreePlan& a,
-                                            const SubtreePlan& b)
-    -> std::optional<SubtreePlan> {
-  // The right subtree has to be a Service.
-  auto bRootOp = std::dynamic_pointer_cast<Service>(b._qet->getRootOperation());
-  if (!static_cast<bool>(bRootOp)) {
-    return std::nullopt;
-  }
-
-  auto serviceWithSibling =
-      makeSubtreePlan(bRootOp->createCopyWithSiblingTree(a._qet));
-  auto qec = bRootOp->getExecutionContext();
-
-  SubtreePlan plan = makeSubtreePlan<Operation>(
-      qec, a._qet, std::move(serviceWithSibling._qet));
-  mergeSubtreePlanIds(plan, a, b);
-
   return plan;
 }
 

src/engine/QueryPlanner.h
@@ -338,19 +338,9 @@ class QueryPlanner {
       SubtreePlan a, SubtreePlan b,
       const std::vector<std::array<ColumnIndex, 2>>& jcs);
 
-  // The following two functions are used to provide a Service-operation with
-  // it's siblingTree, allowing us to optimize the service-query.
-  [[nodiscard]] static std::optional<SubtreePlan> createJoinWithService(
-      const SubtreePlan& a, const SubtreePlan& b,
-      const std::vector<std::array<ColumnIndex, 2>>& jcs);
-
   [[nodiscard]] static std::optional<SubtreePlan> createJoinWithPathSearch(
       const SubtreePlan& a, const SubtreePlan& b,
       const std::vector<std::array<ColumnIndex, 2>>& jcs);
-
-  template <typename Operation>
-  [[nodiscard]] static std::optional<SubtreePlan> createSubtreeWithService(
-      const SubtreePlan& a, const SubtreePlan& b);
 
   // if one of the inputs is a spatial join which is compatible with the other
   // input, then add that other input to the spatial join as a child instead of

src/engine/Service.cpp
@@ -10,6 +10,7 @@
 
 #include "engine/CallFixedSize.h"
 #include "engine/ExportQueryExecutionTrees.h"
+#include "engine/Sort.h"
 #include "engine/VariableToColumnMap.h"
 #include "global/RuntimeParameters.h"
 #include "parser/RdfParser.h"
@@ -23,12 +24,10 @@
 // ____________________________________________________________________________
 Service::Service(QueryExecutionContext* qec,
                  parsedQuery::Service parsedServiceClause,
-                 GetResultFunction getResultFunction,
-                 std::shared_ptr<QueryExecutionTree> siblingTree)
+                 GetResultFunction getResultFunction)
     : Operation{qec},
       parsedServiceClause_{std::move(parsedServiceClause)},
-      getResultFunction_{std::move(getResultFunction)},
-      siblingTree_{std::move(siblingTree)} {}
+      getResultFunction_{std::move(getResultFunction)} {}
 
 // ____________________________________________________________________________
 std::string Service::getCacheKeyImpl() const {
@@ -40,8 +39,8 @@ std::string Service::getCacheKeyImpl() const {
   os << parsedServiceClause_.serviceIri_.toStringRepresentation() << " {\n"
      << parsedServiceClause_.prologue_ << "\n"
      << parsedServiceClause_.graphPatternAsString_ << "\n";
-  if (siblingTree_ != nullptr) {
-    os << siblingTree_->getRootOperation()->getCacheKey() << "\n";
+  if (siblingInfo_.has_value()) {
+    os << siblingInfo_->cacheKey_ << "\n";
   }
   os << "}\n";
   return std::move(os).str();
@@ -302,20 +301,15 @@ Result::Generator Service::computeResultLazily(
 
 // ____________________________________________________________________________
 std::optional<std::string> Service::getSiblingValuesClause() const {
-  if (siblingTree_ == nullptr) {
+  if (!siblingInfo_.has_value()) {
     return std::nullopt;
   }
-
-  const auto& siblingResult = siblingTree_->getResult();
-  if (siblingResult->idTable().size() >
-      RuntimeParameters().get<"service-max-value-rows">()) {
-    return std::nullopt;
-  }
+  const auto& [siblingResult, siblingVars, _] = siblingInfo_.value();
+  AD_CORRECTNESS_CHECK(siblingResult != nullptr);
 
   checkCancellation();
 
   std::vector<ColumnIndex> commonColumnIndices;
-  const auto& siblingVars = siblingTree_->getVariableColumns();
   std::string vars = "(";
   for (const auto& localVar : parsedServiceClause_.visibleVariables_) {
     auto it = siblingVars.find(localVar);
@@ -334,8 +328,7 @@ std::optional<std::string> Service::getSiblingValuesClause() const {
     std::string row = "(";
     for (const auto& columnIdx : commonColumnIndices) {
       const auto& optStr = idToValueForValuesClause(
-          siblingTree_->getRootOperation()->getIndex(),
-          siblingResult->idTable()(rowIndex, columnIdx),
+          getIndex(), siblingResult->idTable()(rowIndex, columnIdx),
           siblingResult->localVocab());
 
       if (!optStr.has_value()) {
@@ -503,4 +496,128 @@ std::optional<std::string> Service::idToValueForValuesClause(
         return RdfEscaping::validRDFLiteralFromNormalized(value);
       }
   }
+}
+
+// ____________________________________________________________________________
+void Service::precomputeSiblingResult(std::shared_ptr<Operation> left,
+                                      std::shared_ptr<Operation> right,
+                                      bool rightOnly, bool requestLaziness) {
+  AD_CORRECTNESS_CHECK(left && right);
+
+  auto skipSortOperation = [](std::shared_ptr<Operation>& op) {
+    if (static_cast<bool>(std::dynamic_pointer_cast<Sort>(op))) {
+      const auto& children = op->getChildren();
+      AD_CORRECTNESS_CHECK(children.size() == 1);
+      op = children[0]->getRootOperation();
+    }
+  };
+  skipSortOperation(left);
+  skipSortOperation(right);
+
+  auto a = std::dynamic_pointer_cast<Service>(left);
+  auto b = std::dynamic_pointer_cast<Service>(right);
+
+  // The sibling is only precomputed iff
+  // - `rightOnly` is true and the right operation is a Service
+  // - or exactly one of the operations is a Service. If we could estimate
+  // the result size of a Service, the Service with the smaller result could
+  // be used as a sibling here.
+  if ((rightOnly && !static_cast<bool>(b)) ||
+      (!rightOnly && static_cast<bool>(a) == static_cast<bool>(b))) {
+    return;
+  }
+
+  const auto& [service, sibling] = [&]() {
+    if (a) {
+      return std::tie(a, right);
+    } else {
+      AD_CORRECTNESS_CHECK(b);
+      return std::tie(b, left);
+    }
+  }();
+  AD_CORRECTNESS_CHECK(service != nullptr);
+
+  auto addRuntimeInfo = [&](bool siblingUsed) {
+    std::string_view v = siblingUsed ? "yes"sv : "no"sv;
+    service->runtimeInfo().addDetail("optimized-with-sibling-result", v);
+    sibling->runtimeInfo().addDetail("used-to-optimize-service-sibling", v);
+  };
+
+  auto siblingResult = sibling->getResult(
+      false, requestLaziness ? ComputationMode::LAZY_IF_SUPPORTED
+                             : ComputationMode::FULLY_MATERIALIZED);
+
+  if (siblingResult->isFullyMaterialized()) {
+    bool resultIsSmall = siblingResult->idTable().size() <=
+                         RuntimeParameters().get<"service-max-value-rows">();
+    if (resultIsSmall) {
+      service->siblingInfo_.emplace(
+          siblingResult, sibling->getExternallyVisibleVariableColumns(),
+          sibling->getCacheKey());
+    }
+    sibling->precomputedResultBecauseSiblingOfService() =
+        std::move(siblingResult);
+    addRuntimeInfo(resultIsSmall);
+    return;
+  }
+
+  // Creates a `Result::Generator` from partially materialized result data.
+  auto partialResultGenerator =
+      [](std::vector<Result::IdTableVocabPair> pairs,
+         Result::Generator prevGenerator,
+         Result::Generator::iterator it) -> Result::Generator {
+    for (auto& pair : pairs) {
+      co_yield pair;
+    }
+    for (auto& pair : std::ranges::subrange{it, prevGenerator.end()}) {
+      co_yield pair;
+    }
+  };
+
+  // Start materializing the lazy `siblingResult`.
+  size_t rows = 0;
+  std::vector<Result::IdTableVocabPair> resultPairs;
+  auto generator = std::move(siblingResult->idTables());
+  const size_t maxValueRows =
+      RuntimeParameters().get<"service-max-value-rows">();
+  for (auto it = generator.begin(); it != generator.end(); ++it) {
+    auto& pair = *it;
+    rows += pair.idTable_.size();
+    resultPairs.push_back(std::move(pair));
+
+    if (rows > maxValueRows) {
+      // Stop precomputation as the size of `siblingResult` exceeds the
+      // threshold it is not useful for the service operation. Pass the
+      // partially materialized result to the sibling.
+      sibling->precomputedResultBecauseSiblingOfService() =
+          std::make_shared<const Result>(
+              partialResultGenerator(std::move(resultPairs),
+                                     std::move(generator), std::move(++it)),
+              siblingResult->sortedBy());
+      addRuntimeInfo(false);
+      return;
+    }
+  }
+
+  // The `siblingResult` has been fully materialized, so it can now be
+  // used in both sibling and service.
+  Result::IdTableVocabPair siblingPair(
+      IdTable{sibling->getResultWidth(),
+              sibling->getExecutionContext()->getAllocator()},
+      LocalVocab{});
+  siblingPair.idTable_.reserve(rows);
+
+  for (auto& pair : resultPairs) {
+    siblingPair.idTable_.insertAtEnd(pair.idTable_);
+    siblingPair.localVocab_.mergeWith(std::span{&pair.localVocab_, 1});
+  }
+
+  service->siblingInfo_.emplace(
+      std::make_shared<Result>(std::move(siblingPair),
+                               siblingResult->sortedBy()),
+      sibling->getExternallyVisibleVariableColumns(), sibling->getCacheKey());
+
+  sibling->precomputedResultBecauseSiblingOfService() =
+      service->siblingInfo_->precomputedResult_;
+  addRuntimeInfo(true);
 }

commit 5166332a086fbf5b9d192170a2fbcdde9b582055

fix join-order s.t. precomputation of service works again. (#1661)
This fixes #1659, an issue introduced in #1556 that made the precomputation of a `SERVICE` sibling in `JOIN` operations impossible.

src/engine/Join.cpp
@@ -159,6 +159,12 @@ ProtoResult Join::computeResult(bool requestLaziness) {
     return createEmptyResult();
   }
 
+  // If one of the RootOperations is a Service, precompute the result of its
+  // sibling.
+  Service::precomputeSiblingResult(_left->getRootOperation(),
+                                   _right->getRootOperation(), false,
+                                   requestLaziness);
+
   // Always materialize results that meet one of the following criteria:
   // * They are already present in the cache
   // * Their result is small
@@ -195,11 +201,6 @@ ProtoResult Join::computeResult(bool requestLaziness) {
     }
   }
 
-  // If one of the RootOperations is a Service, precompute the result of its
-  // sibling.
-  Service::precomputeSiblingResult(_left->getRootOperation(),
-                                   _right->getRootOperation(), false,
-                                   requestLaziness);
   std::shared_ptr<const Result> leftRes =
       leftResIfCached ? leftResIfCached : _left->getResult(true);
   checkCancellation();