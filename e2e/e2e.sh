qaecy/qlever

commit 32163186a4073c80e684e480502330633016ed8f

Support BM25 and tf-idf scores in the full-text index (#1688)
With this PR, there are now three different types of scores for a word occurence in a document in the full-text index. The score has to be chosen when the text index is built. The scores are as follows:
* Explicitly defined scores in the wordsfile (the only option so far)
* tf-idf scores
* BM25 scores, with configurable parameters `b` and `k`.
Note that the tf-idf and BM25 scores take into consideration both the words- and the docsfile, so they currently have the following behavior:

Only words from the wordsfile become part of the text index, but the occurences of these words  in the docsfile have to be counted to compute the scores. This means that the score values will be wrong (too low) if the words as specified in the wordsfile are not found in the documents because the default tokenizer in QLever is inconsistent with the one used to declare the wordsfile.

In the future we will implement a configurable tokenizer for the docsfile, to make it thus easier to mitigate the above caveat or to get rid of the explicit wordsfile at all.

src/index/Index.cpp
@@ -25,9 +25,10 @@ void Index::createFromOnDiskIndex(const std::string& onDiskBase) {
 }
 
 // ____________________________________________________________________________
-void Index::addTextFromContextFile(const std::string& contextFile,
-                                   bool addWordsFromLiterals) {
-  pimpl_->addTextFromContextFile(contextFile, addWordsFromLiterals);
+void Index::buildTextIndexFile(
+    std::optional<std::pair<std::string, std::string>> wordsAndDocsFile,
+    bool addWordsFromLiterals) {
+  pimpl_->buildTextIndexFile(wordsAndDocsFile, addWordsFromLiterals);
 }
 
 // ____________________________________________________________________________
@@ -213,6 +214,12 @@ void Index::setSettingsFile(const std::string& filename) {
 // ____________________________________________________________________________
 void Index::setNumTriplesPerBatch(uint64_t numTriplesPerBatch) {
   return pimpl_->setNumTriplesPerBatch(numTriplesPerBatch);
+}
+
+// ____________________________________________________________________________
+void Index::storeTextScoringParamsInConfiguration(
+    TextScoringMetric scoringMetric, float b, float k) {
+  return pimpl_->storeTextScoringParamsInConfiguration(scoringMetric, b, k);
 }
 
 // ____________________________________________________________________________

src/index/Index.h
@@ -13,6 +13,7 @@
 #include "index/InputFileSpecification.h"
 #include "index/Permutation.h"
 #include "index/StringSortComparator.h"
+#include "index/TextScoringEnum.h"
 #include "index/Vocabulary.h"
 #include "parser/TripleComponent.h"
 #include "util/CancellationHandle.h"
@@ -61,7 +62,8 @@ class Index {
     // Stores the index of the entity of each result.
     vector<Id> eids_;
     // Stores for each result how often an entity
-    // appears in its associated TextRecord.
+    // appears in its associated TextRecord. [[OLD DEFINITION]]
+    // Now scores BM25 scores for all words that are in the voacabulary
     vector<Score> scores_;
   };
 
@@ -94,8 +96,9 @@ class Index {
 
   // Add a text index to a complete KB index. First read the given context
   // file (if file name not empty), then add words from literals (if true).
-  void addTextFromContextFile(const std::string& contextFile,
-                              bool addWordsFromLiterals);
+  void buildTextIndexFile(
+      std::optional<std::pair<std::string, std::string>> wordsAndDocsFile,
+      bool addWordsFromLiterals);
 
   // Build docsDB file from given file (one text record per line).
   void buildDocsDB(const std::string& docsFile);
@@ -205,6 +208,9 @@ class Index {
   void setSettingsFile(const std::string& filename);
 
   void setNumTriplesPerBatch(uint64_t numTriplesPerBatch);
+
+  void storeTextScoringParamsInConfiguration(TextScoringMetric scoringMetric,
+                                             float b, float k);
 
   const std::string& getTextName() const;
 

src/index/IndexBuilderMain.cpp
@@ -155,6 +155,7 @@ int main(int argc, char** argv) {
   string textIndexName;
   string kbIndexName;
   string settingsFile;
+  string scoringMetric = "explicit";
   std::vector<string> filetype;
   std::vector<string> inputFile;
   std::vector<string> defaultGraphs;
@@ -164,6 +165,8 @@ int main(int argc, char** argv) {
   bool keepTemporaryFiles = false;
   bool onlyPsoAndPos = false;
   bool addWordsFromLiterals = false;
+  float bScoringParam = 0.75;
+  float kScoringParam = 1.75;
   std::optional<ad_utility::MemorySize> stxxlMemory;
   std::optional<ad_utility::MemorySize> parserBufferSize;
   optind = 1;
@@ -214,6 +217,17 @@ int main(int argc, char** argv) {
   add("add-text-index,A", po::bool_switch(&onlyAddTextIndex),
       "Only build the text index. Assumes that a knowledge graph index with "
       "the same `index-basename` already exists.");
+  add("bm25-b", po::value(&bScoringParam),
+      "Sets the b param in the BM25 scoring metric for the fulltext index."
+      " This has to be between (including) 0 and 1.");
+  add("bm25-k", po::value(&kScoringParam),
+      "Sets the k param in the BM25 scoring metric for the fulltext index."
+      "This has to be greater than or equal to 0.");
+  add("set-scoring-metric,S", po::value(&scoringMetric),
+      "Sets the scoring metric used. Options are \"explicit\" for explicit "
+      "scores that are read from the wordsfile, "
+      "\"tf-idf\" for tf idf "
+      "and \"bm25\" for bm25. The default is count.");
 
   // Options for the knowledge graph index.
   add("settings-file,s", po::value(&settingsFile),
@@ -245,6 +259,14 @@ int main(int argc, char** argv) {
       return EXIT_SUCCESS;
     }
     po::notify(optionsMap);
+    if (kScoringParam < 0) {
+      throw std::invalid_argument("The value of bm25-k must be >= 0");
+    }
+    if (bScoringParam < 0 || bScoringParam > 1) {
+      throw std::invalid_argument(
+          "The value of bm25-b must be between and "
+          "including 0 and 1");
+    }
   } catch (const std::exception& e) {
     std::cerr << "Error in command-line argument: " << e.what() << '\n';
     std::cerr << boostOptions << '\n';
@@ -329,9 +351,25 @@ int main(int argc, char** argv) {
       AD_CONTRACT_CHECK(!fileSpecifications.empty());
       index.createFromFiles(fileSpecifications);
     }
+    bool wordsAndDocsFileSpecified = !(wordsfile.empty() || docsfile.empty());
 
-    if (!wordsfile.empty() || addWordsFromLiterals) {
-      index.addTextFromContextFile(wordsfile, addWordsFromLiterals);
+    if (!(wordsAndDocsFileSpecified ||
+          (wordsfile.empty() && docsfile.empty()))) {
+      throw std::runtime_error(absl::StrCat(
+          "Only specified ", wordsfile.empty() ? "docsfile" : "wordsfile",
+          ". Both or none of docsfile and wordsfile have to be given to build "
+          "text index. If none are given the option to add words from literals "
+          "has to be true. For details see --help."));
+    }
+    if (wordsAndDocsFileSpecified || addWordsFromLiterals) {
+      index.storeTextScoringParamsInConfiguration(
+          getTextScoringMetricFromString(scoringMetric), bScoringParam,
+          kScoringParam);
+      index.buildTextIndexFile(
+          wordsAndDocsFileSpecified
+              ? std::optional{std::pair{wordsfile, docsfile}}
+              : std::nullopt,
+          addWordsFromLiterals);
     }
 
     if (!docsfile.empty()) {

commit 3e95e506684622e26bc75c43274ab9b94fbb8836

Fix `IndexBuilderMain --add-text-index` (#1945)
`IndexBuilderMain` provides an option `--add-text-index` for adding a text index to an already build triple index. This was broken by #1688, which overwrites `<name>.meta-data.json` and then complains that information is missing from that file. This is now fixed. Also add an E2E test for this option.

.github/workflows/native-build.yml
@@ -100,5 +100,12 @@ jobs:
       - name: Running and printing the benchmark examples.
         run: ${{github.workspace}}/build/benchmark/BenchmarkExamples -p
 
-      - name: E2E
+      # Run the E2E test only once, but for GCC add the text index separately.
+      # Note that the dispatch by the compiler is arbitrary, we just want
+      # to have both cases tested.
+      - name: E2E with index in one go
+        if: (matrix.compiler == 'clang')
         run: ${{github.workspace}}/e2e/e2e.sh
+      - name: E2E with separate text index build
+        if: (matrix.compiler != 'clang')
+        run: ${{github.workspace}}/e2e/e2e.sh -t

src/engine/Result.cpp
@@ -216,6 +216,9 @@ void Result::checkDefinedness(const VariableToColumnMap& varColMap) {
         std::move(idTables()),
         [varColMap = varColMap, performCheck = std::move(performCheck)](
             Result::IdTableVocabPair& pair) {
+          // The lambda capture is only required when expensive checks are
+          // enabled.
+          (void)performCheck;
           AD_EXPENSIVE_CHECK(performCheck(varColMap, pair.idTable_));
           return std::move(pair);
         }};

src/index/Index.cpp
@@ -28,8 +28,10 @@ void Index::createFromOnDiskIndex(const std::string& onDiskBase,
 // ____________________________________________________________________________
 void Index::buildTextIndexFile(
     const std::optional<std::pair<std::string, std::string>>& wordsAndDocsFile,
-    bool addWordsFromLiterals) {
-  pimpl_->buildTextIndexFile(wordsAndDocsFile, addWordsFromLiterals);
+    bool addWordsFromLiterals, TextScoringMetric textScoringMetric,
+    std::pair<float, float> bAndKForBM25) {
+  pimpl_->buildTextIndexFile(wordsAndDocsFile, addWordsFromLiterals,
+                             textScoringMetric, bAndKForBM25);
 }
 
 // ____________________________________________________________________________
@@ -215,12 +217,6 @@ void Index::setSettingsFile(const std::string& filename) {
 // ____________________________________________________________________________
 void Index::setNumTriplesPerBatch(uint64_t numTriplesPerBatch) {
   return pimpl_->setNumTriplesPerBatch(numTriplesPerBatch);
-}
-
-// ____________________________________________________________________________
-void Index::storeTextScoringParamsInConfiguration(
-    TextScoringMetric scoringMetric, float b, float k) {
-  return pimpl_->storeTextScoringParamsInConfiguration(scoringMetric, b, k);
 }
 
 // ____________________________________________________________________________

src/index/Index.h
@@ -102,7 +102,9 @@ class Index {
   void buildTextIndexFile(
       const std::optional<std::pair<std::string, std::string>>&
           wordsAndDocsFile,
-      bool addWordsFromLiterals);
+      bool addWordsFromLiterals,
+      TextScoringMetric textScoringMetric = TextScoringMetric::EXPLICIT,
+      std::pair<float, float> bAndKForBM25 = {0.75f, 1.75f});
 
   // Build docsDB file from given file (one text record per line).
   void buildDocsDB(const std::string& docsFile);
@@ -212,9 +214,6 @@ class Index {
   void setSettingsFile(const std::string& filename);
 
   void setNumTriplesPerBatch(uint64_t numTriplesPerBatch);
-
-  void storeTextScoringParamsInConfiguration(TextScoringMetric scoringMetric,
-                                             float b, float k);
 
   const std::string& getTextName() const;
   const std::string& getKbName() const;

src/index/IndexBuilderMain.cpp
@@ -362,14 +362,12 @@ int main(int argc, char** argv) {
           "has to be true. For details see --help."));
     }
     if (wordsAndDocsFileSpecified || addWordsFromLiterals) {
-      index.storeTextScoringParamsInConfiguration(
-          getTextScoringMetricFromString(scoringMetric), bScoringParam,
-          kScoringParam);
       index.buildTextIndexFile(
           wordsAndDocsFileSpecified
               ? std::optional{std::pair{wordsfile, docsfile}}
               : std::nullopt,
-          addWordsFromLiterals);
+          addWordsFromLiterals, getTextScoringMetricFromString(scoringMetric),
+          {bScoringParam, kScoringParam});
     }
 
     if (!docsfile.empty()) {

src/index/IndexImpl.Text.cpp
@@ -119,7 +119,8 @@ void IndexImpl::logEntityNotFound(const string& word,
 // _____________________________________________________________________________
 void IndexImpl::buildTextIndexFile(
     const std::optional<std::pair<string, string>>& wordsAndDocsFile,
-    bool addWordsFromLiterals) {
+    bool addWordsFromLiterals, TextScoringMetric textScoringMetric,
+    std::pair<float, float> bAndKForBM25) {
   AD_CORRECTNESS_CHECK(wordsAndDocsFile.has_value() || addWordsFromLiterals);
   LOG(INFO) << std::endl;
   LOG(INFO) << "Adding text index ..." << std::endl;
@@ -149,6 +150,10 @@ void IndexImpl::buildTextIndexFile(
   LOG(DEBUG) << "Reloading the RDF vocabulary ..." << std::endl;
   vocab_ = RdfsVocabulary{};
   readConfiguration();
+  {
+    auto [b, k] = bAndKForBM25;
+    storeTextScoringParamsInConfiguration(textScoringMetric, b, k);
+  }
   vocab_.readFromFile(onDiskBase_ + VOCAB_SUFFIX);
 
   scoreData_ = {vocab_.getLocaleManager(), textScoringMetric_,
@@ -796,12 +801,20 @@ auto IndexImpl::getTextBlockMetadataForWordOrPrefix(const std::string& word)
 void IndexImpl::storeTextScoringParamsInConfiguration(
     TextScoringMetric scoringMetric, float b, float k) {
   configurationJson_["text-scoring-metric"] = scoringMetric;
-  if (0 <= b && b <= 1 && 0 <= k) {
-    configurationJson_["b-and-k-parameter-for-text-scoring"] =
-        std::make_pair(b, k);
-  } else {
-    configurationJson_["b-and-k-parameter-for-text-scoring"] =
-        std::make_pair(0.75, 1.75);
-  }
+  textScoringMetric_ = scoringMetric;
+  auto bAndK = [b, k, this]() {
+    if (0 <= b && b <= 1 && 0 <= k) {
+      return std::pair{b, k};
+    } else {
+      if (textScoringMetric_ == TextScoringMetric::BM25) {
+        throw std::runtime_error{absl::StrCat(
+            "Invalid values given for BM25 score: `b=", b, "` and `k=", k,
+            "`, `b` must be in [0, 1] and `k` must be >= 0 ")};
+      }
+      return std::pair{0.75f, 1.75f};
+    }
+  }();
+  bAndKParamForTextScoring_ = bAndK;
+  configurationJson_["b-and-k-parameter-for-text-scoring"] = bAndK;
   writeConfiguration();
 }

src/index/IndexImpl.h
@@ -267,7 +267,8 @@ class IndexImpl {
   // called with the pair empty and bool false
   void buildTextIndexFile(
       const std::optional<std::pair<string, string>>& wordsAndDocsFile,
-      bool addWordsFromLiterals);
+      bool addWordsFromLiterals, TextScoringMetric textScoringMetric,
+      std::pair<float, float> bAndKForBM25);
 
   // Build docsDB file from given file (one text record per line).
   void buildDocsDB(const string& docsFile) const;
@@ -442,9 +443,6 @@ class IndexImpl {
   void setNumTriplesPerBatch(uint64_t numTriplesPerBatch) {
     numTriplesPerBatch_ = numTriplesPerBatch;
   }
-
-  void storeTextScoringParamsInConfiguration(TextScoringMetric scoringMetric,
-                                             float b, float k);
 
   const string& getTextName() const { return textMeta_.getName(); }
   const string& getKbName() const { return pso_.getKbName(); }
@@ -801,6 +799,9 @@ class IndexImpl {
   static void updateInputFileSpecificationsAndLog(
       std::vector<Index::InputFileSpecification>& spec,
       std::optional<bool> parallelParsingSpecifiedViaJson);
+
+  void storeTextScoringParamsInConfiguration(TextScoringMetric scoringMetric,
+                                             float b, float k);
 };
 
 #endif  // QLEVER_SRC_INDEX_INDEXIMPL_H

e2e/e2e.sh
@@ -21,13 +21,15 @@ function print_usage {
   echo "Options:"
   echo "  -i  Use index from the given directory (which must be the root directory of the working copy of QLever, not the e2e_data subdirectory)"
   echo "  -d  Directory of the QLever binaries (relative to the main directory), default: 'build'"
+  echo "  -t  Build the text index with a separate explicit call to `IndexBuilderMain`"
 }
 
 REBUILD_THE_INDEX="YES"
 INDEX_DIRECTORY="." #if not set, we will build the index ourselves.
 BINARY_DIRECTORY="build"
+BUILD_TEXT_INDEX_SEPARATELY="NO"
 
-while getopts ":i:d:" arg; do
+while getopts ":i:d:t" arg; do
   case ${arg} in
     i)
       echo "The index will not be rebuilt"
@@ -36,6 +38,9 @@ while getopts ":i:d:" arg; do
     ;;
     d)
       BINARY_DIRECTORY="${OPTARG}"
+    ;;
+    t)
+      BUILD_TEXT_INDEX_SEPARATELY="YES"
     ;;
   \?) echo "Invalid option: -$OPTARG exiting" >&2
       print_usage
@@ -96,14 +101,31 @@ if [ ${REBUILD_THE_INDEX} == "YES" ] || ! [ -f "${INDEX}.index.pso" ]; then
 
 	rm -f "$INDEX.*"
 	pushd "$BINARY_DIR"
-	echo "Building index $INDEX"
-	./IndexBuilderMain -i "$INDEX" \
-	    -F ttl \
-	    -f "$INPUT.nt" \
-	    -s "$PROJECT_DIR/e2e/e2e-build-settings.json" \
-	    -w "$INPUT.wordsfile.tsv" \
-            -W \
-	    -d "$INPUT.docsfile.tsv" || bail "Building Index failed"
+
+  if [ ${BUILD_TEXT_INDEX_SEPARATELY} == "NO" ]; then
+    echo "Building index $INDEX"
+    ./IndexBuilderMain -i "$INDEX" \
+        -F ttl \
+        -f "$INPUT.nt" \
+        -s "$PROJECT_DIR/e2e/e2e-build-settings.json" \
+        -w "$INPUT.wordsfile.tsv" \
+              -W \
+        -d "$INPUT.docsfile.tsv" || bail "Building Index failed"
+	else
+    echo "Building index $INDEX without text index"
+    ./IndexBuilderMain -i "$INDEX" \
+        -F ttl \
+        -f "$INPUT.nt" \
+        -s "$PROJECT_DIR/e2e/e2e-build-settings.json" \
+        || bail "Building Index failed"
+    echo "Adding text index"
+    ./IndexBuilderMain -A -i "$INDEX" \
+        -s "$PROJECT_DIR/e2e/e2e-build-settings.json" \
+        -w "$INPUT.wordsfile.tsv" \
+              -W \
+        -d "$INPUT.docsfile.tsv" || bail "Building Index failed"
+
+	fi
 	popd
 fi
 

test/util/IndexTestHelpers.cpp
@@ -194,16 +194,31 @@ Index makeTestIndex(const std::string& indexBasename,
     qlever::InputFileSpecification spec{inputFilename, indexType, std::nullopt};
     index.createFromFiles({spec});
     if (createTextIndex) {
-      if (scoringMetric.has_value()) {
-        if (!bAndKParam.has_value()) {
-          index.storeTextScoringParamsInConfiguration(scoringMetric.value(),
-                                                      0.75, 1.75);
-        } else {
-          index.storeTextScoringParamsInConfiguration(
-              scoringMetric.value(), bAndKParam.value().first,
-              bAndKParam.value().second);
-        }
+      // First test the case of invalid b and k parameters for BM25, it should
+      // throw
+      AD_EXPECT_THROW_WITH_MESSAGE(
+          index.buildTextIndexFile(std::nullopt, true, TextScoringMetric::BM25,
+                                   {2.0f, 0.5f}),
+          ::testing::HasSubstr("Invalid values"));
+      AD_EXPECT_THROW_WITH_MESSAGE(
+          index.buildTextIndexFile(std::nullopt, true, TextScoringMetric::BM25,
+                                   {0.5f, -1.0f}),
+          ::testing::HasSubstr("Invalid values"));
+      scoringMetric = scoringMetric.value_or(TextScoringMetric::EXPLICIT);
+      bAndKParam = bAndKParam.value_or(std::pair{0.75f, 1.75f});
+
+      // The following tests that garbage values for b and k work if these
+      // parameters are unnecessary because we don't use `BM25`.
+      if (scoringMetric.value() != TextScoringMetric::BM25) {
+        bAndKParam = std::pair{-3.f, -3.f};
       }
+      auto buildTextIndex = [&index, &scoringMetric, &bAndKParam](
+                                auto wordsAndDocsfile,
+                                bool addWordsFromLiterals) {
+        index.buildTextIndexFile(std::move(wordsAndDocsfile),
+                                 addWordsFromLiterals, scoringMetric.value(),
+                                 bAndKParam.value());
+      };
       if (contentsOfWordsFileAndDocsFile.has_value()) {
         // Create and write to words- and docsfile to later build a full text
         // index from them
@@ -218,20 +233,13 @@ Index makeTestIndex(const std::string& indexBasename,
         index.setKbName(indexBasename);
         index.setTextName(indexBasename);
         index.setOnDiskBase(indexBasename);
-        if (addWordsFromLiterals) {
-          index.buildTextIndexFile(
-              std::pair<std::string, std::string>{indexBasename + ".wordsfile",
-                                                  indexBasename + ".docsfile"},
-              true);
-        } else {
-          index.buildTextIndexFile(
-              std::pair<std::string, std::string>{indexBasename + ".wordsfile",
-                                                  indexBasename + ".docsfile"},
-              false);
-        }
+        buildTextIndex(
+            std::pair<std::string, std::string>{indexBasename + ".wordsfile",
+                                                indexBasename + ".docsfile"},
+            addWordsFromLiterals);
         index.buildDocsDB(indexBasename + ".docsfile");
       } else if (addWordsFromLiterals) {
-        index.buildTextIndexFile(std::nullopt, true);
+        buildTextIndex(std::nullopt, true);
       }
     }
   }